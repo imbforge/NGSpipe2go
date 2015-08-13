d0f157d00343204e675ce633855e3cf350cce924
Oliver Drechsel <o.drechsel@imb-mainz.de>
2015-08-13 16:38:00 +0200

    added collectBpipe as last missing step in git repo

---
+++ collectBpipeLogs/collectBpipeLogs.sh
@@ -0,0 +1,27 @@
+PROJECT=$1
+LOGS=$2
+
+for f in ${PROJECT}/.bpipe/outputs/*; 
+do 
+ echo $f
+ F=$(basename $f)
+ #echo "FILEname " $F
+ JOB=$(echo $F | cut -d. -f1)
+ #echo "JOB " ${JOB}
+ ID=$(grep -E "^commandId" $f | cut -d= -f2)
+ #echo "ID " $ID
+ FILE=$(grep -E "^outputFile" $f|cut -d= -f2) 
+ FILE=$(basename ${FILE})                       
+ echo "JOB: ${JOB}, ID: ${ID}, FILE: ${FILE}, DIR: ${PROJECT}"
+ if [ ! -d "${LOGS}/${JOB}" ]; 
+ then
+  echo "mkdir ${LOGS}/${JOB}"
+  mkdir -p ${LOGS}/${JOB}
+ fi
+
+ if [ -e ${PROJECT}/.bpipe/commandtmp/${ID}/cmd.err ]; 
+ then
+  echo "${PROJECT}/.bpipe/commandtmp/${ID}/cmd.err --> ${LOGS}/${JOB}/${FILE}.log"
+  cp -v ${PROJECT}/.bpipe/commandtmp/${ID}/cmd.err ${LOGS}/${JOB}/${FILE}.log
+ fi
+done 
