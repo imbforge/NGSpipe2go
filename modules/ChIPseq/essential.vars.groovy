af22e6953218f7b3c58aa03fc56be8928a49b4ab
Oliver Drechsel <o.drechsel@imb-mainz.de>
2015-08-14 14:28:29 +0200

    added essential.vars for ChIPseq

---
+++ modules/ChIPseq/essential.vars.groovy
@@ -0,0 +1,18 @@
+ESSENTIAL_PROJECT="" // put project folder here
+ESSENTIAL_BOWTIE_REF="/fsimb/groups/imb-bioinfocf/common-data/igenomes_reference/Mus_musculus/UCSC/mm9/Sequence/BowtieIndex/genome"
+ESSENTIAL_CHR_SIZES=ESSENTIAL_PROJECT + "/mm9.chrom.sizes"
+ESSENTIAL_SAMPLE_PREFIX="Sample_imb_gcf_2014_07_"
+ESSENTIAL_BSGENOME="BSgenome.Mmusculus.UCSC.mm9"
+ESSENTIAL_FRAGLEN=200
+ESSENTIAL_READLEN=50
+ESSENTIAL_MACS2_GSIZE="mm"
+
+//global vars that will be reused in some global vars
+PROJECT=ESSENTIAL_PROJECT
+LOGS=PROJECT + "/logs"
+MAPPED=PROJECT + "/mapped"
+QC=PROJECT + "/qc"
+REPORTS=PROJECT + "/reports"
+RESULTS=PROJECT + "/results"
+TMP=PROJECT + "/tmp"
+TRACKS=PROJECT + "/tracks"
