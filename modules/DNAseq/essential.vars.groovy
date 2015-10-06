3e7b223217438147e70cfe2dc018c503cd6223d1
Oliver Drechsel <o.drechsel@imb-mainz.de>
2015-10-06 15:12:09 +0200

    added ESSENTIAL_CALL_REGION to make time saving possible

--- modules/DNAseq/essential.vars.groovy
+++ modules/DNAseq/essential.vars.groovy
@@ -6,6 +6,7 @@
 ESSENTIAL_PROJECT="/fsimb/groups/imb-bioinfocf/projects/cfb_internal/imbforge/test/dnaseq"          // "/fsimb/groups/imb-bioinfocf/projects/cfb_internal/imb_cf_2013_04_sayols_infrastructure_pipelines/test"
 ESSENTIAL_BWA_REF="/fsimb/groups/imb-bioinfocf/common-data/igenomes_reference/Homo_sapiens/UCSC/hg19/Sequence/BWAIndex/genome.fa"        // "/fsimb/groups/imb-bioinfocf/common-data/star_genomes/mm9/"
 ESSENTIAL_GATK_REF=""
+ESSENTIAL_CALL_REGION="/fsimb/groups/imb-bioinfocf/projects/cfb_internal/imbforge/test/dnaseq/call.region.bed" // OPTIONALLY limit variant calling to a limited region, e.g. exonic sequences or chr3 only. Will save a lot of compute time.
 ESSENTIAL_PAIRED="yes"                   // paired end design
 ESSENTIAL_SAMPLE_PREFIX="NA12877_"     // "Sample_imb_richly_2014_05_"
 ESSENTIAL_KNOWN_VARIANTS="/fsimb/groups/imb-bioinfocf/common-data/GATK_resources/2.8/hg19/dbsnp_138.hg19.vcf" // KnownVariants could dbSNP from GATK resource bundle. This is crucial for BaseQualityRecalibration step! vcf file needs to be unzipped
