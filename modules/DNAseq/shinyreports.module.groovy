9d79cd5ed8305ca13eae07136705e68f884ecd63
Oliver Drechsel <o.drechsel@imb-mainz.de>
2015-10-07 18:10:07 +0200

    adding BWA_SUFFIX

--- modules/DNAseq/shinyreports.module.groovy
+++ modules/DNAseq/shinyreports.module.groovy
@@ -4,46 +4,52 @@ shinyReports = {
 	doc title: "shinyReports",
 		desc:  "creates the source code to compile the shiny and markdown reports",
 		constraints: "",
-		author: "Sergi Sayols"
-
-	exec """
-		if [ ! -d "${REPORTS}" ]; then
-			mkdir -p ${REPORTS};
-		fi &&
-		
-		cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/server.R ${REPORTS}                &&
-		cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/ui.R ${REPORTS}                    &&
-		cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/DE.shinyrep.helpers.R ${REPORTS}   &&
-		cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/bustard.pl ${REPORTS}              &&
-		cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/BustardSummary.toMD.xsl ${REPORTS} &&
-		
-		if [ -e "${REPORTS}/DEreport.Rmd" ]; then
-			echo 'DEreport.Rmd already exists. Older copy will be kept and not overwritten';
-		else
-			cp ${TOOL_DEPENDENCIES}/imb-forge/reports/rnaseq/DEreport.Rmd ${REPORTS};
-		fi &&
-		
-		PROJECT=\$(basename ${SHINYREPS_PROJECT})                            &&
-		sed -i "2,2s/SHINYREPS_PROJECT/\${PROJECT}/" ${REPORTS}/DEreport.Rmd &&
-		
-		echo "SHINYREPS_PROJECT=${SHINYREPS_PROJECT}" >  ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_ORG=${SHINYREPS_ORG}"         >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_DB=${SHINYREPS_DB}"           >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_LOG=${SHINYREPS_LOG}"         >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_QC=${SHINYREPS_QC}"           >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_RES=${SHINYREPS_RES}"         >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_PREFIX=${SHINYREPS_PREFIX}"   >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_STAR_LOG=${SHINYREPS_STAR_LOG}"       >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_STAR_SUFFIX=${SHINYREPS_STAR_SUFFIX}" >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_STARparms_SUFFIX=${SHINYREPS_STARparms_SUFFIX}" >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_FASTQC_LOG=${SHINYREPS_FASTQC_LOG}"   >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_DUPRADAR_LOG=${SHINYREPS_DUPRADAR_LOG}" >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_RNATYPES_LOG=${SHINYREPS_RNATYPES_LOG}" >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_GENEBODYCOV_LOG=${SHINYREPS_GENEBODYCOV_LOG}" >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_BUSTARD=${SHINYREPS_BUSTARD}"         >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_DE_EDGER=${SHINYREPS_DE_EDGER}"       >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_SUBREAD=${SHINYREPS_SUBREAD}"       >> ${REPORTS}/shinyReports.txt &&
-		echo "SHINYREPS_SUBREAD_SUFFIX=${SHINYREPS_SUBREAD_SUFFIX}"       >> ${REPORTS}/shinyReports.txt
-	""","shinyReports"
+		author: "Oliver Drechsel"
+	
+	output.dir = REPORTS
+	
+	produce("shinyReports.txt") {
+		exec """
+			
+			cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/server.R ${REPORTS}                &&
+			cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/ui.R ${REPORTS}                    &&
+			cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/variant.shinyrep.helpers.R ${REPORTS}   &&
+			cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/bustard.pl ${REPORTS}              &&
+			cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/BustardSummary.toMD.xsl ${REPORTS} &&
+			
+			if [ -e "${REPORTS}/variantreport.Rmd" ]; then
+				echo 'variantreport.Rmd already exists. Older copy will be kept and not overwritten';
+			else
+				cp ${MODULE_FOLDER}/../tools/reports/shiny_dnaseq_reporting_tool/variantreport.Rmd ${REPORTS};
+			fi &&
+			
+			PROJECT=\$(basename ${SHINYREPS_PROJECT})                            &&
+			sed -i "2,2s/SHINYREPS_PROJECT/\${PROJECT}/" ${REPORTS}/variantreport.Rmd &&
+			
+			echo "SHINYREPS_PROJECT=${SHINYREPS_PROJECT}" >  $output &&
+			echo "SHINYREPS_ORG=${SHINYREPS_ORG}"         >> $output &&
+			echo "SHINYREPS_DB=${SHINYREPS_DB}"           >> $output &&
+			echo "SHINYREPS_LOG=${SHINYREPS_LOG}"         >> $output &&
+			echo "SHINYREPS_QC=${SHINYREPS_QC}"           >> $output &&
+			echo "SHINYREPS_RES=${SHINYREPS_RES}"         >> $output &&
+			echo "SHINYREPS_PREFIX=${SHINYREPS_PREFIX}"   >> $output &&
+			echo "SHINYREPS_FASTQC_LOG=${SHINYREPS_FASTQC_LOG}"     >> $output &&
+			echo "SHINYREPS_BWA_LOG=${SHINYREPS_BWA_LOG}"           >> $output &&
+			echo "SHINYREPS_BWA_SUFFIX=${SHINYREPS_BWA_SUFFIX}"     >> $output &&
+			
+			
+			echo "SHINYREPS_STAR_LOG=${SHINYREPS_STAR_LOG}"       >> $output &&
+			echo "SHINYREPS_STAR_SUFFIX=${SHINYREPS_STAR_SUFFIX}" >> $output &&
+			echo "SHINYREPS_STARparms_SUFFIX=${SHINYREPS_STARparms_SUFFIX}" >> $output &&
+			echo "SHINYREPS_DUPRADAR_LOG=${SHINYREPS_DUPRADAR_LOG}" >> $output &&
+			echo "SHINYREPS_RNATYPES_LOG=${SHINYREPS_RNATYPES_LOG}" >> $output &&
+			echo "SHINYREPS_GENEBODYCOV_LOG=${SHINYREPS_GENEBODYCOV_LOG}" >> $output &&
+			echo "SHINYREPS_BUSTARD=${SHINYREPS_BUSTARD}"         >> $output &&
+			echo "SHINYREPS_DE_EDGER=${SHINYREPS_DE_EDGER}"       >> $output &&
+			echo "SHINYREPS_SUBREAD=${SHINYREPS_SUBREAD}"         >> $output &&
+			echo "SHINYREPS_SUBREAD_SUFFIX=${SHINYREPS_SUBREAD_SUFFIX}"  >> $output &&
+			echo "SHINYREPS_PLOTS_COLUMN=${SHINYREPS_PLOTS_COLUMN}"      >> $output
+		""","shinyReports"
+	}
 }
 
--- modules/DNAseq/shinyreports.vars.groovy
+++ modules/DNAseq/shinyreports.vars.groovy
@@ -6,15 +6,19 @@ SHINYREPS_LOG=LOGS			//where the logs lie
 SHINYREPS_QC=QC				//where the QC lie
 SHINYREPS_RES=RESULTS		//where the results lie
 SHINYREPS_PREFIX=ESSENTIAL_SAMPLE_PREFIX	//standard sample prefix
-SHINYREPS_STAR_LOG=LOGS + "/STAR_se"	//where the STAR logs lie
-SHINYREPS_STAR_SUFFIX="Log.final.out"	//extension given to the STAR log files
-SHINYREPS_STARparms_SUFFIX="Log.out"	//extension given to the STAR log files
 SHINYREPS_FASTQC_LOG=FASTQC_OUTDIR		//where the Fastqc logs lie
-SHINYREPS_DUPRADAR_LOG=DUPRADAR_OUTDIR	//where the dupRadar logs lie
-SHINYREPS_RNATYPES_LOG=QC + "/RNAtypes"	//where the RNAtypes logs lie
-SHINYREPS_GENEBODYCOV_LOG=GENEBODYCOV_OUTDIR //where the geneBodyCov logs lie
-SHINYREPS_BUSTARD=QC + "/DemultiplexedBustardSummary.xml"	//where the bustard xml file lies
-SHINYREPS_DE_EDGER=DE_edgeR_OUTDIR + "/DE_edgeR.RData"   //where the DE_edgeR output lies
-SHINYREPS_SUBREAD=RESULTS + "/subread-count" // location of the subread counts
-SHINYREPS_SUBREAD_SUFFIX=".raw_readcounts.tsv.summary" // the extension of the subread stats file
+SHINYREPS_BWA_LOG=LOGS + "/BWA_pe"	//where the BWA (samtools flagstat) logs lie
+SHINYREPS_BWA_SUFFIX=".bam.log"	//extension given to the BWA log files
+
+//SHINYREPS_STAR_LOG=LOGS + "/STAR_se"	//where the STAR logs lie
+//SHINYREPS_STAR_SUFFIX="Log.final.out"	//extension given to the STAR log files
+//SHINYREPS_STARparms_SUFFIX="Log.out"	//extension given to the STAR log files
+//
+//SHINYREPS_DUPRADAR_LOG=DUPRADAR_OUTDIR	//where the dupRadar logs lie
+//SHINYREPS_RNATYPES_LOG=QC + "/RNAtypes"	//where the RNAtypes logs lie
+//SHINYREPS_GENEBODYCOV_LOG=GENEBODYCOV_OUTDIR //where the geneBodyCov logs lie
+//SHINYREPS_BUSTARD=QC + "/DemultiplexedBustardSummary.xml"	//where the bustard xml file lies
+//SHINYREPS_DE_EDGER=DE_edgeR_OUTDIR + "/DE_edgeR.RData"   //where the DE_edgeR output lies
+//SHINYREPS_SUBREAD=RESULTS + "/subread-count" // location of the subread counts
+//SHINYREPS_SUBREAD_SUFFIX=".raw_readcounts.tsv.summary" // the extension of the subread stats file
 
