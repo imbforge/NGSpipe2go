ada56fb318e2d17974b2b808e1c884b40657fee1
Sergi Sayols <s.sayolspuig@imb-mainz.de>
2015-11-03 12:02:24 +0100

    update default folders in essential.vars.groovy. Fix Picard version in tool.locations.groovy

--- modules/ChIPseq/essential.vars.groovy
+++ modules/ChIPseq/essential.vars.groovy
@@ -1,6 +1,6 @@
-ESSENTIAL_PROJECT="" // put project folder here
-ESSENTIAL_BOWTIE_REF="/fsimb/groups/imb-bioinfocf/common-data/igenomes_reference/Mus_musculus/UCSC/mm9/Sequence/BowtieIndex/genome"
-ESSENTIAL_SAMPLE_PREFIX="Sample_imb_gcf_2014_07_"
+ESSENTIAL_PROJECT="/project" // put project folder here
+ESSENTIAL_BOWTIE_REF="/data/igenomes_reference/Mus_musculus/UCSC/mm9/Sequence/BowtieIndex/genome"
+ESSENTIAL_SAMPLE_PREFIX="Sample_"
 ESSENTIAL_BSGENOME="BSgenome.Mmusculus.UCSC.mm9"
 ESSENTIAL_FRAGLEN=200
 ESSENTIAL_READLEN=50
