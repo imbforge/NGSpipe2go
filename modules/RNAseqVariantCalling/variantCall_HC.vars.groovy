3f6d64fddf980efa4ba38e1c55d19ffccc195578
Antonio Domingues <amjdomingues@gmail.com>
2016-01-13 16:37:08 +0100

    added variant call via HaplotypeCaller

---
+++ modules/RNAseqVariantCalling/variantCall_HC.vars.groovy
@@ -0,0 +1,8 @@
+// vars for task BaseRecalibration step from catalog RNAseqVariantCalling, version 1
+OUTPUT_HC=RESULTS +  "/HC"
+GATK_REF=ESSENTIAL_GENOME_REF
+VCF_REF=ESSENTIAL_VCF_REF
+GATK_THREADS=ESSENTIAL_THREADS
+MIN_SCORE_CALL=20
+MIN_SCORE_EMIT=20
+
