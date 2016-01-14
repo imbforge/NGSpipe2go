933147376e3872c1b7f8916504e41c082ed5e8b8
Antonio Domingues <amjdomingues@gmail.com>
2016-01-14 17:06:00 +0100

    fixed cluster settings - wall time not enough

--- modules/RNAseqVariantCalling/variant_filtration.vars.groovy
+++ modules/RNAseqVariantCalling/variant_filtration.vars.groovy
@@ -1,6 +1,5 @@
-// vars for task VariantFIltration step from catalog RNAseqVariantCalling, version 1
+// vars for task VariantFiltration step from catalog RNAseqVariantCalling, version 1
 OUTPUT_HC=RESULTS +  "/HC"
 GATK_REF=ESSENTIAL_GENOME_REF
-GATK_THREADS=ESSENTIAL_THREADS
 
 
