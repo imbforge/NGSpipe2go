d7cf2ad0b647338fd3ee2658ab23d4593c66eedf
Sergi Sayols <s.sayolspuig@imb-mainz.de>
2016-03-17 14:34:02 +0100

    dupradar output dir

--- modules/RNAseq/dupradar.module.groovy
+++ modules/RNAseq/dupradar.module.groovy
@@ -5,7 +5,7 @@ dupRadar = {
 		bpipe_version: "tested with bpipe 0.9.8.7",
 		author: "Sergi Sayols"
 
-	output.dir = QC + "/dupradar"
+	output.dir = QC + "/dupRadar"
 	def DUPRADAR_FLAGS = DUPRADAR_GTF      + " " +
 	                     DUPRADAR_STRANDED + " " + 
 			    		 DUPRADAR_PAIRED   + " " +
