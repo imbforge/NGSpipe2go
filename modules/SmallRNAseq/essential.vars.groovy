b2b827436ce0767a50d49a7b2d56cd3bafa9d0ad
Antonio Domingues <amjdomingues@gmail.com>
2016-07-08 16:21:45 +0200

    Options added to essential.vars: read length to keep; settings for ce

--- modules/SmallRNAseq/essential.vars.groovy
+++ modules/SmallRNAseq/essential.vars.groovy
@@ -2,25 +2,31 @@
 //By: ssayolsp At: Fr 17 Okt 2014 17:12:41 CEST
 //
 // REMEMBER TO CHANGE THESE ESSENTIAL VARS!!
-//
+
+// ZEBRAFISH
 ESSENTIAL_PROJECT="/local/scratch1/imb-kettinggr/adomingues/projects/bpipe_small_rna"
 ESSENTIAL_BOWTIE_REF="/home/adomingu/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/BowtieIndexChr/chr"
 ESSENTIAL_GENOME_REF="/home/adomingu/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/chr_sequences/chr.clean.fa"
-
 // vars for piRNA analyis
 ESSENTIAL_FEATURES="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/pipeline/features.bed"
-
 // vars for mirDeep2
 ESSENTIAL_MATURE_MIRNA="~/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/SmallRNA/mature.noSpaces.fa"
 ESSENTIAL_HAIRPIN_MIRNA="~/imb-git addkettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/SmallRNA/hairpin.noSpaces.fa"
-
 // read size to keep (added 8 bp to account for barcodes)
-// // Zebrafish
-// MIN_LENGTH=21
-// MAX_LENGTH=51
-// C. elegans
-MIN_LENGTH=26
-MAX_LENGTH=38
+// Zebrafish
+MIN_LENGTH=21
+MAX_LENGTH=51
+
+
+// C. ELEGANS
+// ESSENTIAL_PROJECT="/local/scratch1/imb-kettinggr/adomingues/projects/bpipe_small_rna"
+// ESSENTIAL_BOWTIE_REF="/home/adomingu/imb-kettinggr/genomes/Caenorhabditis_elegans/UCSC/ce10/Sequence/BowtieIndex/genome"
+// ESSENTIAL_GENOME_REF="/home/adomingu/imb-kettinggr/genomes/Caenorhabditis_elegans/UCSC/ce10/Sequence/BowtieIndex/genome.fa"
+// // vars for piRNA analyis
+// ESSENTIAL_FEATURES="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/pipeline/features.bed"
+// // // read size to keep (added 8 bp to account for barcodes)
+// MIN_LENGTH=26
+// MAX_LENGTH=38
 
 //global vars that will be reused in some global vars
 PROJECT=ESSENTIAL_PROJECT
