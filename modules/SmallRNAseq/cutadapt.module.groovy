d6dc4c8a92898f9f0ee9f2d5efa9c3c7ba141083
Antonio Domingues <amjdomingues@gmail.com>
2016-07-19 17:06:39 +0200

    syntax modifications. Functionality unchanged

--- modules/SmallRNAseq/cutadapt.module.groovy
+++ modules/SmallRNAseq/cutadapt.module.groovy
@@ -1,22 +1,20 @@
 Cutadapt = {
-	doc title: "Cutadapt",
-		desc:  "remove adapter from reads",
-		constraints: "Only supports compressed FASTQ files",
-		author: "Antonio Domingues"
+   doc title: "Cutadapt",
+      desc:  "remove adapter from reads",
+      constraints: "Only supports compressed FASTQ files",
+      author: "Antonio Domingues"
 
-	output.dir = CUTADAPT_OUTDIR
+   output.dir = CUTADAPT_OUTDIR
 
-	transform(".fastq.gz") to (".cutadapt.fastq.gz") {
+   transform(".fastq.gz") to (".cutadapt.fastq.gz") {
       exec """
          if [ -n "\$LSB_JOBID" ]; then
-				export TMPDIR=/jobdir/\${LSB_JOBID};
-			fi &&
-
-			echo 'VERSION INFO'  1>&2 &&
-			${TOOL_CUTADAPT} --version 1>&2 &&
-			echo '/VERSION INFO' 1>&2 &&
-
-			${TOOL_CUTADAPT} $ADAPTER_SEQUENCE -O $MINIMUM_OVERLAP -m $MINIMUM_LENGTH_KEEP -M $MAXIMUM_LENGTH_KEEP -o $output $input 2>&1 >> ${FASTQ_QUALITY_FILTER_OUTDIR}/cutadapt.log
-		""","Cutadapt"
-	}
+            export TMPDIR=/jobdir/\${LSB_JOBID};
+         fi &&
+         echo 'VERSION INFO'  1>&2 &&
+         ${TOOL_CUTADAPT} --version 1>&2 &&
+         echo '/VERSION INFO' 1>&2 &&
+         ${TOOL_CUTADAPT} $ADAPTER_SEQUENCE -O $MINIMUM_OVERLAP -m $MINIMUM_LENGTH_KEEP -M $MAXIMUM_LENGTH_KEEP -o $output $input 2>&1 >> ${FASTQ_QUALITY_FILTER_OUTDIR}/cutadapt.log
+      ""","Cutadapt"
+   }
 }
