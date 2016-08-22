f198ea6a98750507e95ba811791148cba1cf61f2
Sergi Sayols <s.sayolspuig@imb-mainz.de>
2016-08-22 11:47:16 +0200

    default RNAtypes type column set to gene_biotype (to match latest versions of ensembl)

--- modules/RNAseq/rnatypes.vars.groovy
+++ modules/RNAseq/rnatypes.vars.groovy
@@ -9,7 +9,7 @@ RNAtypes_PAIRED="paired=" + ESSENTIAL_PAIRED   // paired end yes|no
 RNAtypes_STRANDED="stranded=" + ESSENTIAL_STRANDED // strandness yes|no|reverse
 RNAtypes_MULTIMAP="multimap=NONE"	        // ALL|NONE|RANDOM
 RNAtypes_FTYPE="ftype=exon"		            // feature type on GTF file: exon|gene|...
-RNAtypes_FTYPECOL="ftypecol=gene_type"      // column name  on GTF file containing the biotype information
+RNAtypes_FTYPECOL="ftypecol=gene_biotype"   // column name  on GTF file containing the biotype information
 RNAtypes_CORES="cores=" + Integer.toString(ESSENTIAL_THREADS) // number of cores to use
 RNAtypes_EXTRA=""
 
