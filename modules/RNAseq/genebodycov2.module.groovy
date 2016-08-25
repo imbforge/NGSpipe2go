44c0acdcca5ea6467ae231be6dddb9e5e804b9f0
Sergi Sayols <s.sayolspuig@imb-mainz.de>
2016-08-25 14:31:25 +0200

    add the reimplementation of geneBodyCoverage

---
+++ modules/RNAseq/genebodycov2.module.groovy
@@ -0,0 +1,36 @@
+//rule for task geneBodyCov from catalog RNAseq, version 1
+//desc: Calculate the RNA-seq coverage over gene body
+geneBodyCov2 = {
+	doc title: "geneBodyCoverage2",
+		desc:  """Calculate the RNA-seq coverage over gene body. 
+			Useful to check the 5' or 3' coverage bias""",
+		constraints: "",
+		bpipe_version: "tested with bpipe 0.9.9.9",
+		author: "Sergi Sayols"
+
+    output.dir = GENEBODYCOV2_OUTDIR
+	def GENEBODYCOV2_FLAGS = GENEBODYCOV_GTF      + " " +
+                             GENEBODYCOV_PAIRED   + " " +
+                             GENEBODYCOV_STRANDED + " " +
+                             GENEBODYCOV_OUTDIR   + " " +
+                             GENEBODYCOV_THREADS
+	
+    // run the chunk
+	transform(".bam") to ("_geneBodyCov.png") {
+		exec """
+			export TOOL_DEPENDENCIES=$TOOL_DEPENDENCIES &&
+			source ${TOOL_R}/env.sh &&
+			if [ -n "\$LSB_JOBID" ]; then
+				export TMPDIR=/jobdir/\${LSB_JOBID};
+			fi &&
+			
+			echo 'VERSION INFO'  1>&2 ;
+			echo \$(${TOOL_R}/bin/Rscript --version 2>&1 | cut -d' ' -f5) 1>&2 ;
+			echo '/VERSION INFO'  1>&2 ;
+			
+			${TOOL_R}/bin/Rscript ${TOOL_GENEBODYCOV2}/geneBodyCov.R bam=$input $GENEBODYCOV2_FLAGS &&
+			mv ${GENEBODYCOV2_OUTNAME}.counts.* $output.dir
+		""","geneBodyCov2"
+	}
+	forward input
+}
---
+++ modules/RNAseq/genebodycov2.vars.groovy
@@ -0,0 +1,6 @@
+//vars for task geneBodyCov2
+GENEBODYCOV_GTF="gtf=" + ESSENTIAL_GENESGTF2  // the gencode annotation GTF (can be compressed)
+GENEBODYCOV_PAIRED="paired=" + ESSENTIAL_PAIRED   // paired end yes|no
+GENEBODYCOV_STRANDED="stranded=" + ESSENTIAL_STRANDED // strandness yes|no|reverse
+GENEBODYCOV_OUTDIR="outdir=" + QC + "/geneBodyCov"
+GENEBODYCOV_THREADS="threads=" + Integer.toString(ESSENTIAL_THREADS) // number of cores to use
