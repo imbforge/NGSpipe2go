7448d427d80847ec83ff7c9b2dc1fb0638b8a557
ssayols <sergisayolspuig@gmail.com>
2016-09-13 10:53:25 +0200

    clean star module

--- modules/RNAseq/star.module.groovy
+++ modules/RNAseq/star.module.groovy
@@ -3,8 +3,8 @@
 STAR_se = {
 	doc title: "STAR alignment",
 		desc:  "Align single/paired end reads",
-		constraints: "Only works with compressed input. Set all global vars.",
-		bpipe_version: "tested with bpipe 0.9.8.7",
+		constraints: "Paired end reads expected to have a _R1 _R2 suffix.",
+		bpipe_version: "tested with bpipe 0.9.9.9",
 		author: "Sergi Sayols"
 
 	output.dir = MAPPED
@@ -20,75 +20,73 @@ STAR_se = {
 		F_LOG.mkdirs()
 	}
 	
-    // add paired end support
+    // calculate name of the sample being processed (added paired end support)
 	def OUTPUTFILE = input1
 	int path_index = OUTPUTFILE.lastIndexOf("/")
 	OUTPUTFILE = OUTPUTFILE.substring(path_index+1)
-	println(OUTPUTFILE)
-        if( ESSENTIAL_PAIRED == "yes") {
+
+    if(ESSENTIAL_PAIRED == "yes") {
 		OUTPUTFILE = (OUTPUTFILE =~ /_R1.fastq.gz/).replaceFirst("")
-	}
-	else {
+	} else {
 		OUTPUTFILE = (OUTPUTFILE =~ /.fastq.gz/).replaceFirst("")
 	}
 
+    // star flags
+    def STAR_FLAGS = "--runMode alignReads "        +
+                     "--genomeLoad NoSharedMemory " +
+                     "--outStd SAM "                +
+                     "--outSAMattributes Standard " +
+                     "--outSJfilterReads Unique "   +
+                     "--readFilesCommand zcat "     +
+                     "--outFileNamePrefix " + LOGS + "/STAR_se/" + OUTPUTFILE + " " +
+                     "--outTmpDir " + TMP + "/" + OUTPUTFILE + " " +
+                     STAR_UNMAPPED_BAM + " " +
+                     STAR_UNMAPPED_OUT + " " +
+                     STAR_MAXRAM   + " " +
+                     STAR_BUFSIZE  + " " +
+                     STAR_REF      + " " +
+                     STAR_THREADS  + " " +
+                     STAR_MM       + " " +
+                     STAR_MULTIMAP + " " +
+                     STAR_MININTRO + " " +
+                     STAR_OVERHANG + " " +
+                     STAR_GTF      + " " +
+                     STAR_EXTRA
+
+    // samtools flags
+    def SAMTOOLS_FLAGS = "-bhSu"
+    if(STAR_FILTER_SEC == "YES") {
+        SAMTOOLS_FLAGS = " -F 256 " + SAMTOOLS_FLAGS    //remove secondary alignments
+    }
+
+    def SAMTOOLS_SORT_FLAGS = " -O bam " + STAR_SAMTOOLS_THREADS
+
 	// code chunk
+    // TODO: change to latest or at least try to warn, if the genome index was created using the wrong version of STAR
 	produce(OUTPUTFILE + ".bam", OUTPUTFILE + "Log.final.out") {
-		// flags
-		def SAMPLE = new File(input.prefix.prefix)
-		def STAR_FLAGS = "--runMode alignReads "        +
-                         "--genomeLoad NoSharedMemory " +
-                         "--outStd SAM "                +
-                         "--outSAMattributes Standard " +
-                         "--outSJfilterReads Unique "   +
-                         "--readFilesCommand zcat "     +
-                         "--outFileNamePrefix " + LOGS + "/STAR_se/" + SAMPLE.name + " " +
-                         "--outTmpDir " + TMP + "/" + SAMPLE.name + " " +
-						 STAR_UNMAPPED_BAM + " " +
-						 STAR_UNMAPPED_OUT + " " +
-                         STAR_MAXRAM   + " " +
-                         STAR_BUFSIZE  + " " +
-                         STAR_REF      + " " +
-                         STAR_THREADS  + " " +
-                         STAR_MM       + " " +
-                         STAR_MULTIMAP + " " +
-                         STAR_MININTRO + " " +
-                         STAR_OVERHANG + " " +
-                         STAR_GTF      + " " +
-                         STAR_EXTRA
-
-		def SAMTOOLS_FLAGS = "-bhSu"
-		if(STAR_FILTER_SEC == "YES") {
-			SAMTOOLS_FLAGS = " -F 256 " + SAMTOOLS_FLAGS
-		}
-
-		// TODO: change to latest or at least try to warn, if the genome index was created using the wrong version of STAR
 		exec """
 			export TOOL_DEPENDENCIES=$TOOL_DEPENDENCIES &&
 			source ${TOOL_STAR}/env.sh &&
-			source ${TOOL_MMR}/env.sh &&
 			if [ -n "\$LSB_JOBID" ]; then
 				export TMPDIR=/jobdir/\${LSB_JOBID};
 			else 
 				export TMPDIR=$TMP;
 			fi                                          &&
 			
-			if [ -e $TMP/$SAMPLE.name ];
+			if [ -e $TMP/$OUTPUTFILE ];
 			then
 				echo 'removing old STAR tmp folder';
-				rm -r $TMP/$SAMPLE.name*;
+				rm -r $TMP/$OUTPUTFILE*;
 			fi &&
 			
 			echo 'VERSION INFO'  1>&2 &&
 			echo \$(STAR --version) 1>&2 &&
 			echo '/VERSION INFO' 1>&2 &&
 			
-			STAROUTPUTFILE=\$(basename $output1.prefix) &&
+			STAR $STAR_FLAGS --readFilesIn $inputs | ${TOOL_SAMTOOLS} view $SAMTOOLS_FLAGS - | ${TOOL_SAMTOOLS} sort $SAMTOOLS_SORT_FLAGS -T \${TMPDIR}/${OUTPUTFILE}_sort - > $output1 &&
 			
-			STAR $STAR_FLAGS --readFilesIn $inputs | ${TOOL_SAMTOOLS} view $SAMTOOLS_FLAGS - | ${TOOL_SAMTOOLS} sort -O bam -T \${TMPDIR}/\${STAROUTPUTFILE}_sort $STAR_SAMTOOLS_THREADS - > $output1 &&
-			
-			mv ${LOGS}/STAR_se/${SAMPLE.name}SJ.out.tab $output.dir &&
-			ln -s ${LOGS}/STAR_se/${SAMPLE.name}Log.final.out $output.dir
+			mv ${LOGS}/STAR_se/${OUTPUTFILE}SJ.out.tab $output.dir &&
+			ln -s ${LOGS}/STAR_se/${OUTPUTFILE}Log.final.out $output.dir
 		""","STAR_se"
 	}
 }
