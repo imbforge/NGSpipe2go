7207ca174fe95988daff8566244285995e723ada
ssayols <sergisayolspuig@gmail.com>
2016-09-15 14:24:10 +0200

    automatic output directory create in RNAtypes.R

--- modules/RNAseq/rnatypes.module.groovy
+++ modules/RNAseq/rnatypes.module.groovy
@@ -7,11 +7,11 @@ RNAtypes = {
 		bpipe_version: "tested with bpipe 0.9.8.7",
 		author: "Sergi Sayols"
 
-	output.dir = RNAtypes_OUT.replaceFirst("out=", "")
+	output.dir = RNAtypes_OUTDIR.replaceFirst("out=", "")
 	def RNAtypes_FLAGS = RNAtypes_FOLDER   + " " +
 	                     RNAtypes_PATTERN  + " " +
 	                     RNAtypes_GTF      + " " +
-	                     RNAtypes_OUT      + " " +
+	                     RNAtypes_OUTDIR   + "/" + RNAtypes_OUTNAME + " " +
 	                     RNAtypes_PRE      + " " +
 	                     RNAtypes_SUF      + " " +
 	                     RNAtypes_PAIRED   + " " +
@@ -37,8 +37,7 @@ RNAtypes = {
 			echo \$(${TOOL_R}/bin/Rscript --version 2>&1 | cut -d' ' -f5) 1>&2 ;
 			echo '/VERSION INFO'  1>&2 ;
 			
-			${TOOL_R}/bin/Rscript ${TOOL_RNAtypes}/RNAtypes.R $RNAtypes_FLAGS &&
-			mv ${RNAtypes_OUTNAME}.counts.* $output.dir
+			${TOOL_R}/bin/Rscript ${TOOL_RNAtypes}/RNAtypes.R $RNAtypes_FLAGS
 		""","RNAtypes"
 	}
 }
