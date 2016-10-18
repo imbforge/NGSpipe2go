7db0b4f5589738c65442cac425a119a94b1327bd
Antonio Domingues <amjdomingues@gmail.com>
2016-10-18 11:38:38 +0200

    added bigwig for each strand. ESSENTIAL_GENOME_SIZE var defined in essencial vars (smRNA-seq)

---
+++ modules/SmallRNAseq/bamCoverageStrands.module.groovy
@@ -0,0 +1,33 @@
+//rule for task Bam2bw from catalog NGS, version 1
+//desc: Create scaled bigwig tracks from a bam file
+BamCoverageStrands = {
+	doc title: "BamCoverage",
+		desc:  "Convert BAM file to bigWig. The --filterRNAstrand option assumes the sequencing library generated from ILLUMINA dUTP/NSR/NNSR methods",
+		constraints: "Requires deepTools >= 2.2",
+		author: "Antonio Domingues"
+
+	output.dir=TRACKS
+
+	transform(".bam") to (".fwd.scaled.bw", ".rev.scaled.bw") {
+     	exec """
+	if [ -n "\$LSB_JOBID" ]; then
+		export TMPDIR=/jobdir/\${LSB_JOBID};
+	fi &&
+
+	source ${TOOL_PYTHONENV} &&
+
+	bamCoverage --bam $input -o $output1 \
+		--normalizeTo1x $GENOME_SIZE \
+		--binSize 10 \
+		--numberOfProcessors $ESSENTIAL_CORES \
+		--filterRNAstrand forward &&
+
+	bamCoverage --bam $input -o $output1 \
+		--normalizeTo1x $GENOME_SIZE \
+		--binSize 10 \
+		--numberOfProcessors $ESSENTIAL_CORES \
+		--filterRNAstrand reverse 
+		""","BamCoverageStrands"
+	}
+}
+
