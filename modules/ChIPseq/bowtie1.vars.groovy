69a4bb31be9039549af33e916970dfa67ba08fd2
Emil Karaulanov <e.karaulanov@imb-mainz.de>
2016-10-19 11:51:20 +0200

    mapping parameters optimisation

--- modules/ChIPseq/bowtie1.vars.groovy
+++ modules/ChIPseq/bowtie1.vars.groovy
@@ -1,13 +1,13 @@
-// bowtie parameters
+// bowtie parameters with suggested typical defaults
 BOWTIE_THREADS=" -p" + Integer.toString(ESSENTIAL_THREADS) // threads to use
 BOWTIE_REF=ESSENTIAL_BOWTIE_REF // prefix of the bowtie reference genome
-BOWTIE_INSERT="-l 28"			// seed size. Match with fragment size
-BOWTIE_MM_SEED="-n 2"			// number of mismatches allowed in the seed
-BOWTIE_MAQERR="-e 70"			// max sum of quals for -n mismatches
-BOWTIE_MULTIMAP="-m 1"			// discard reads mapping to more than MULTIMAP positions
-BOWTIE_BEST="--best --strata --chunkmbs 256"	// bowtie best mode
+BOWTIE_INSERT="-l 40"			// seed length, the optimum depends on the read length and quality
+BOWTIE_MM_SEED="-n 2"			// maximum number of mismatches allowed in the seed sequence
+BOWTIE_MAQERR="-e 70"			// maximum permitted total of quality values at all mismatched positions throughout the entire alignment
+BOWTIE_MULTIMAP="-m 1"			// discard (-m 1) or keep one random alignment (-M 1) of all reads mapping to multiple locations
+BOWTIE_BEST="--best --strata --tryhard --chunkmbs 256"	// bowtie best mapping mode
 BOWTIE_QUALS="--phred33-quals"	// phred33-quals. Use --phred64-quals for old sequencing runs
-BOWTIE_EXTRA=""                 // extra parms to be passed to bowtie
+BOWTIE_EXTRA=""                 // extra parms to be passed to bowtie, e.g. for trimming barcodes
 
 // samtools parameters
 SAMTOOLS_THREADS="-@" + Integer.toString(ESSENTIAL_THREADS)
