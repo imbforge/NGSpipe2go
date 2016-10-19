dba70903325a50ea43f705ca6cda74952ee5e3b6
nkreim <n.kreim@imb-mainz.de>
2016-10-19 12:52:04 +0200

    Initial comit of the bpipe1pe module untested has to be adjusted to the current settings

---
+++ modules/ChIPseq/bowtie1pe.module.groovy
@@ -0,0 +1,47 @@
+//rule for task bowtie_se from catalog ChIPseq, version 1
+//desc: Align single end reads
+bowtie_pe = {
+	doc title: "Bowtie PE alignment",
+		desc:  "Align paired end reads",
+		constraints: "Only works with compressed input. Samtools multithreaded version expected (>=0.1.19).",
+		bpipe_version: "tested with bpipe 0.9.8.7",
+		author: "Sergi Sayols modified for paired end by Nastasja Kreim"
+
+	output.dir = BOWTIE_MAPPED
+	def OUTPUTFILE = input1
+	int path_index = OUTPUTFILE.lastIndexOf("/")
+	OUTPUTFILE = OUTPUTFILE.substring(path_index+1)
+	println(OUTPUTFILE)
+	OUTPUTFILE = (OUTPUTFILE =~ /_read1.fastq.gz/).replaceFirst("")
+
+	def BOWTIE_FLAGS = "-q --sam "  +
+                       BOWTIE_QUALS    + " " + 
+                       BOWTIE_BEST     + " " + 
+                       BOWTIE_MM_SEED  + " " + 
+                       BOWTIE_INSERT   + " " + 
+                       BOWTIE_MAQERR   + " " + 
+                       BOWTIE_MULTIMAP + " " + 
+                       BOWTIE_THREADS  + " " + 
+                       BOWTIE_EXTRA
+
+	produce(OUTPUTFILE + ".bam") {
+		exec """
+			export TOOL_DEPENDENCIES=$TOOL_DEPENDENCIES &&
+			source ${TOOL_BOWTIE}/env.sh   &&
+			if [ -n "\$LSB_JOBID" ]; then
+				export TMPDIR=/jobdir/\${LSB_JOBID};
+			fi                                          &&
+			echo $output;
+			echo 'VERSION INFO'  1>&2 ;
+			echo \$(bowtie --version | grep bowtie | cut -d' ' -f3)   1>&2 ;
+			echo '/VERSION INFO' 1>&2 ;
+			base1=`basename $input1`;
+			base2=`basename $input2`;
+			zcat $input1 > \$TMPDIR/\${base1%.gz};
+			zcat $input2 > \$TMPDIR/\${base2%.gz};
+			
+			bowtie $BOWTIE_FLAGS $BOWTIE_REF -1 $TMPDIR/\${base1%.gz} -2 $TMPDIR/\${base2%.gz} | ${TOOL_SAMTOOLS} view -bhSu - | ${TOOL_SAMTOOLS} sort -@ $BOWTIE_SAMTOOLS_THREADS - $output.prefix
+		""","bowtie_pe"
+	}
+}
+
---
+++ modules/ChIPseq/bowtie1pe.vars.groovy
@@ -0,0 +1,11 @@
+BOWTIE_THREADS=" -p" + Integer.toString(ESSENTIAL_THREADS) // threads to use
+BOWTIE_REF=ESSENTIAL_BOWTIE_REF // prefix of the bowtie reference genome
+BOWTIE_INSERT="-l 22"			// seed size. Match with fragment size
+BOWTIE_MM_SEED="-n 1"			// number of mismatches allowed in the seed
+BOWTIE_MAQERR=""			// max sum of quals for -n mismatches
+BOWTIE_MULTIMAP="-m 1"			// discard reads mapping to more than MULTIMAP positions
+BOWTIE_BEST="--best --strata --chunkmbs 256"	// bowtie best mode, this does not apply to paired end
+BOWTIE_QUALS="--phred33-quals"	// phred33-quals. Use --phred64-quals for old sequencing runs
+BOWTIE_EXTRA="--fr"                 // extra parms to be passed to bowtie
+BOWTIE_SAMTOOLS_THREADS=Integer.toString(ESSENTIAL_THREADS)
+BOWTIE_MAPPED=MAPPED + "/bowtie1"
