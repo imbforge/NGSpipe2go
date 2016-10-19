fede3a9ea9a37caa60b1b6b57398993d8b15545d
Oliver Drechsel <o.drechsel@imb-mainz.de>
2016-10-19 18:41:18 +0200

    adjusted output folder

--- modules/RNAseq/subread2rnatypes.vars.groovy
+++ modules/RNAseq/subread2rnatypes.vars.groovy
@@ -4,7 +4,7 @@ RNATYPES_GENESGTF="-a " + ESSENTIAL_GENESGTF
 RNATYPES_FEATURE="-t exon" // type of feature that is to be counted in
 RNATYPES_ACCUMULATE="-g " + ESSENTIAL_FEATURETYPE // type of annotation counts should be accumulated on. Usually that would be gene_id, but in this case we choose gene_biotype
 RNATYPES_CORES="-T " + Integer.toString(ESSENTIAL_THREADS)
-RNATYPES_OUTDIR=RESULTS + "/rnatypes-count"
+RNATYPES_OUTDIR=QC + "/rnatypes-count"
 
 RNATYPES_EXTRA=""    // extra parms to sent to the tool
 
