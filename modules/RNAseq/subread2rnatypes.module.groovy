59fbf2130a63bc8743b97d7354354a2796cd7616
Oliver Drechsel <o.drechsel@imb-mainz.de>
2016-10-19 18:17:34 +0200

    adding a new version of RNAtypes

---
+++ modules/RNAseq/subread2rnatypes.module.groovy
@@ -0,0 +1,52 @@
+//rule for task feature_count from Subread package, version 1
+//desc: Counting reads in features with featureCounts
+
+// example content of env.sh
+// export PATH=${TOOL_DEPENDENCIES}/subread/1.5.0/bin:$PATH
+
+rnatypes = {
+	doc title: "subread_count",
+	desc:  "Counting gene biotypes in features with feature-count out of the subread package",
+	constraints: """Default: strand specific counting.""",
+	bpipe_version: "tested with bpipe 0.9.8.7",
+	author: "Oliver Drechsel"
+	
+	output.dir  = RNATYPES_OUTDIR
+	def RNATYPES_FLAGS = "-F GTF " +
+						RNATYPES_GENESGTF + " " +
+						RNATYPES_FEATURE + " " +
+						RNATYPES_ACCUMULATE + " " +
+					    RNATYPES_CORES    + " " +
+                        RNATYPES_EXTRA    + " "
+						
+	
+	if(RNATYPES_PAIRED == "yes") {
+		RNATYPES_FLAGS = "-p " + RNATYPES_FLAGS
+	}
+	
+	// no|yes|reverse
+	if(RNATYPES_STRANDED == "no") {
+		RNATYPES_FLAGS = "-s 0 " + RNATYPES_FLAGS
+	}
+	else if (RNATYPES_STRANDED == "yes") {
+		RNATYPES_FLAGS = "-s 1 " + RNATYPES_FLAGS
+	}
+	else {
+		RNATYPES_FLAGS = "-s 2 " + RNATYPES_FLAGS
+	}
+	
+	// run the chunk
+	transform(".bam") to (".readcounts.tsv") {
+		exec """
+			export TOOL_DEPENDENCIES=$TOOL_DEPENDENCIES &&
+			source ${TOOL_SUBREAD}/env.sh &&
+			
+			echo 'VERSION INFO'  1>&2 ;
+			echo \$(featureCounts 2>&1 | grep Version | cut -d' ' -f2) 1>&2 ;
+			echo '/VERSION INFO' 1>&2 ;
+			
+			featureCounts $RNATYPES_FLAGS -o $output $input 2> ${output.prefix}_rnatypeslog.stderr
+	
+		""","rnatypes"
+	}
+}
---
+++ modules/RNAseq/subread2rnatypes.vars.groovy
@@ -0,0 +1,10 @@
+RNATYPES_STRANDED=ESSENTIAL_STRANDED//whether the data is from a strand-specific assay (illumina SR: always reverse)
+RNATYPES_PAIRED=ESSENTIAL_PAIRED	 //paired end design
+RNATYPES_GENESGTF="-a " + ESSENTIAL_GENESGTF
+RNATYPES_FEATURE="-t exon" // type of feature that is to be counted in
+RNATYPES_ACCUMULATE="-g " + ESSENTIAL_FEATURETYPE // type of annotation counts should be accumulated on. Usually that would be gene_id, but in this case we choose gene_biotype
+RNATYPES_CORES="-T " + Integer.toString(ESSENTIAL_THREADS)
+RNATYPES_OUTDIR=RESULTS + "/rnatypes-count"
+
+RNATYPES_EXTRA=""    // extra parms to sent to the tool
+
