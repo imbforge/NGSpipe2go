9a8a53fbcc73ffbac4890cdeeda9ab9d276d633f
nkreim <n.kreim@imb-mainz.de>
2016-10-19 12:54:09 +0200

    Initial commit for rmats module. The module is untested.

---
+++ modules/RNAseq/prermats.vars.groovy
@@ -0,0 +1,5 @@
+PREMATS_OUTDIR=RESULTS + "/preMATS"
+PREMATS_TARGETS="targets_rMATS.txt" //the rmats targets file differs from the normal targets file by supplying and fragment size for your samples in the last column
+PREMATS_CONTRASTS="contrasts.txt"
+PREMATS_POSTFIX="_targets_rMats.txt"
+
