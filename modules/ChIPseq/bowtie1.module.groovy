7f4d940572eb358dee4616a585f564bf5dfcc1ae
ssayols <sergisayolspuig@gmail.com>
2016-11-14 14:34:03 +0100

    add semicolon at the end of some lines

--- modules/ChIPseq/bowtie1.module.groovy
+++ modules/ChIPseq/bowtie1.module.groovy
@@ -33,8 +33,8 @@ bowtie_se = {
 			echo '/VERSION INFO' 1>&2 ;
 
             if [ ! -e $TMP ]; then
-                mkdir -p $TMP
-            fi
+                mkdir -p $TMP;
+            fi &&
 			
 			zcat $input | bowtie $BOWTIE_FLAGS $BOWTIE_REF - | samtools view $SAMTOOLS_VIEW_FLAGS - | samtools sort $SAMTOOLS_SORT_FLAGS -T $TMP/\$(basename $output.prefix)_bowtie1_sort - > $output
 		""","bowtie_se"
