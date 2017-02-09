e332cd86f9b1e79773bd7ddf146bd6395b081224
nkreim <n.kreim@imb-mainz.de>
2017-02-09 15:35:25 +0100

    adjustment for sammtools in bowtie1pe

--- modules/ChIPseq/bowtie1pe.module.groovy
+++ modules/ChIPseq/bowtie1pe.module.groovy
@@ -24,6 +24,8 @@ bowtie_pe = {
                        BOWTIE_THREADS  + " " + 
                        BOWTIE_EXTRA
 
+	def SAMTOOLS_VIEW_FLAGS = "-bhSu "
+	def SAMTOOLS_SORT_FLAGS = "-O bam " + SAMTOOLS_THREADS
 	produce(OUTPUTFILE + ".bam") {
 		exec """
 			module load bowtie/${BOWTIE_VERSION} &&
@@ -31,14 +33,13 @@ bowtie_pe = {
             if [ -n "\$LSB_JOBID" ]; then
 				export TMPDIR=/jobdir/\${LSB_JOBID};
 			fi                                          &&
-			echo $output;
+			echo $output &&
 			
-            base1=`basename $input1`;
-			base2=`basename $input2`;
-			zcat $input1 > \$TMPDIR/\${base1%.gz};
-			zcat $input2 > \$TMPDIR/\${base2%.gz};
-			
-			bowtie $BOWTIE_FLAGS $BOWTIE_REF -1 $TMPDIR/\${base1%.gz} -2 $TMPDIR/\${base2%.gz} | samtools view -bhSu - | samtools sort -@ $BOWTIE_SAMTOOLS_THREADS - $output.prefix
+            base1=`basename $input1` &&
+			base2=`basename $input2` &&
+			zcat $input1 > \$TMPDIR/\${base1%.gz} &&
+			zcat $input2 > \$TMPDIR/\${base2%.gz} &&			
+			bowtie $BOWTIE_FLAGS $BOWTIE_REF -1 $TMPDIR/\${base1%.gz} -2 $TMPDIR/\${base2%.gz} | samtools view $SAMTOOLS_VIEW_FLAGS - | samtools sort $SAMTOOLS_SORT_FLAGS -T$TMPDIR/\$(basename $output.prefix)_bowtie1_sort - > $output
 		""","bowtie_pe"
 	}
 }
