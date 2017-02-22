a3058db31b1c2d1bd4b333d4b204e4c67afe2144
ssayols <sergisayolspuig@gmail.com>
2017-02-22 17:59:53 +0100

    load module filbowtie2unique in the chipseq_pe pipeline. Other minor modifications

--- modules/ChIPseq/bowtie2pe.vars.groovy
+++ modules/ChIPseq/bowtie2pe.vars.groovy
@@ -1,8 +1,8 @@
-BOWTIE2_THREADS=" -p 12" //+ Integer.toString(ESSENTIAL_THREADS) // threads to use
+BOWTIE2_THREADS=" -p " + Integer.toString(ESSENTIAL_THREADS) // threads to use
 BOWTIE2_SAMTOOLS_THREADS=Integer.toString(ESSENTIAL_THREADS) // threads to use
-BOWTIE2_REF=" -x " + ESSENTIAL_BOWTIE2_REF // prefix of the bowtie reference genome
+BOWTIE2_REF=" -x " + ESSENTIAL_BOWTIE_REF // prefix of the bowtie reference genome
 BOWTIE2_INSERT=""			// seed size. Match with fragment size, the default 20 is set by --very-sensitive
-BOWTIE2_MM_SEED="-N 1"			// number of mismatches allowed in the seed
+BOWTIE2_MM_SEED="-N 1"		// number of mismatches allowed in the seed
 BOWTIE2_QUALS="--phred33"	// phred33-quals. Use --phred64-quals for old sequencing runs
 BOWTIE2_EXTRA="--fr --very-sensitive --end-to-end --maxins 1000 --minins 0"
 BOWTIE2_MAPPED=MAPPED
