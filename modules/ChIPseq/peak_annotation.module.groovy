d0d09f473be859c2511ab367ba6256d5ea8a60be
sridewi1 <sridewi1@imbc1.imb.uni-mainz.de>
2017-02-23 15:20:48 +0100

    add new peak annotation module

---
+++ modules/ChIPseq/peak_annotation.module.groovy
@@ -0,0 +1,27 @@
+peak_annotation = {
+
+    doc title: "peak_annotation",
+        desc: "ChIP peak annotation, comparison and visualization using ChIPSeeker package",
+        constraints: "",
+        bpipe_version:"",
+        author:"Sri Dewi"
+
+    output.dir=peak_annotation_OUTDIR.replaceFirst("out=","")
+    def peak_annotation_FLAGS = peak_annotation_FILES + " " +
+        peak_annotation_TRANSCRIPT_TYPE + " " + 
+        peak_annotation_TRANSCRIPT_DB + " " + 
+        peak_annotation_ORGDB + " " + 
+        peak_annotation_REGIONTSS + " " + 
+        peak_annotation_OUTDIR + " " +
+        peak_annotation_EXTRA
+
+
+    produce("Peak_Annotation.RData") {
+        exec """
+        module load R/${R_VERSION} &&
+
+            Rscript ${TOOL_PEAK_ANNOTATION}/Peak_Annotation.R $peak_annotation_FLAGS;
+
+        ""","peak_annotation"
+    }
+}
