f75c23d93f04720a6142f8dbeb50b2d74da70f69
gpetrosi <gpetrosi@imbc1.imb.uni-mainz.de>
2017-03-02 16:05:06 +0100

    fix typo in variable name in the subread.vars.groovy

--- modules/RNAseq/subread.vars.groovy
+++ modules/RNAseq/subread.vars.groovy
@@ -1,7 +1,7 @@
-SUBREAD_STRANDED=ESSENTIAL_STRANDED//whether the data is from a strand-specific assay (illumina SR: always reverse)
+SUBREAD_STRANDED=ESSENTIAL_STRANDED //whether the data is from a strand-specific assay (illumina SR: always reverse)
 SUBREAD_PAIRED=ESSENTIAL_PAIRED	 //paired end design
 SUBREAD_DUPLICATES="--ignoreDup" //set the parm to ignore duplicates
-SUBREAD_GENESGTF="-a " + ESSENTIAL_GENES
+SUBREAD_GENESGTF="-a " + ESSENTIAL_GENESGTF
 SUBREAD_CORES="-T " + Integer.toString(ESSENTIAL_THREADS)
 SUBREAD_OUTDIR=RESULTS + "/subread-count"
 SUBREAD_EXTRA=""    // extra parms to sent to the tool
