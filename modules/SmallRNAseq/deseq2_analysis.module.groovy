df752954bd2e9c07317f15ccba6eae0ade222f62
Antonio Domingues <amjdomingues@gmail.com>
2017-04-03 13:43:54 +0200

    some change

---
+++ modules/SmallRNAseq/deseq2_analysis.module.groovy
@@ -0,0 +1,23 @@
+PerformDEGAnalaysis = {
+   doc title: "PerformDEGAnalaysis",
+   desc: "Creates DEG report",
+   constraints: "needs a table with gene names and biotypes",
+   author: "António Domingues"
+
+   output.dir = HTSEQCOUNT_OUT
+
+   produce("normlization_factors.txt"){
+      exec """
+         if [ -n "\$LSB_JOBID" ]; then
+            export TMPDIR=/jobdir/\${LSB_JOBID};
+         fi &&
+
+         cd $output.dir
+         ln -s $DEG_SCRIPT .
+         ln -s $DEG_SCRIPT_HELPERS .
+
+         knit_to_html.sh $DEG_SCRIPT 
+         
+      ""","PerformDEGAnalaysis"
+   }
+}
