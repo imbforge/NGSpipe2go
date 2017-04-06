059d184292b54c8d40f9e3762701ec6c60b19ce5
ankbusch <a.busch@imb-mainz.de>
2017-04-06 11:06:34 +0200

    moving input of adapter sequence from cutadapt module to essentials

--- modules/smallRNAseq_BCF/cutadapt.vars.groovy
+++ modules/smallRNAseq_BCF/cutadapt.vars.groovy
@@ -1,7 +1,7 @@
 //vars for task Cutadapt from catalog smallRNAseq_BCF, version 0.1
 
 CUTADAPT_OUTDIR=PROCESSED
-ADAPTER_SEQUENCE="-a TGGAATTCTCGGGTGCCAAGG"  // adapter to be trimmed off
+ADAPTER_SEQUENCE="-a " + ESSENTIAL_ADAPTER_SEQUENCE  // adapter to be trimmed off
 MINIMUM_OVERLAP=ESSENTIAL_MINADAPTEROVERLAP  // minimal overlap of the read and the adapter
 MINIMUM_LENGTH_KEEP=ESSENTIAL_MINREADLENGTH  // minimal length of reads to be kept
 MAXIMUM_LENGTH_KEEP=Integer.toString(ESSENTIAL_READLENGTH - ESSENTIAL_MINADAPTEROVERLAP) // maximal length of reads to be kept for further analysis
