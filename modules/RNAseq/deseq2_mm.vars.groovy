01c64a56b7b23d1f50eef4410e32d0ec286a3e5d
Emil Karaulanov <e.karaulanov@imb-mainz.de>
2017-05-09 17:44:23 +0200

    text edit

--- modules/RNAseq/deseq2_mm.vars.groovy
+++ modules/RNAseq/deseq2_mm.vars.groovy
@@ -1,7 +1,7 @@
 DE_DESeq2_MM_TARGETS=DE_DESeq2_TARGETS       //targets file. Check the bin directory for the format
 DE_DESeq2_MM_CONTRASTS=DE_DESeq2_CONTRASTS   //contrasts file. Check the bin directory for the format
 DE_DESeq2_MM_MMATRIX=DE_DESeq2_MMATRIX       //formula for the linear model
-DE_DESeq2_MM_FILTER=DE_DESeq2_FILTER         //whether to filter invariant genes
+DE_DESeq2_MM_FILTER=DE_DESeq2_FILTER         //whether to perform automatic independent filtering of lowly expressed genes
 DE_DESeq2_MM_PREFIX=DE_DESeq2_PREFIX         //prefix to be removed from file names
 DE_DESeq2_MM_SUFFIX=DE_DESeq2_SUFFIX         //suffix to be removed from file names
 DE_DESeq2_MM_DUPRADAR_OUTDIR=DUPRADAR_OUTDIR.replaceFirst("outdir=", "") //where the dupradar left the output tables
