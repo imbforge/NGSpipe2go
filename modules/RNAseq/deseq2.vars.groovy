8603bfa3ad34d90e4f5d6ff5efb530a79ccd6eab
Emil Karaulanov <e.karaulanov@imb-mainz.de>
2017-05-09 17:44:13 +0200

    text edit

--- modules/RNAseq/deseq2.vars.groovy
+++ modules/RNAseq/deseq2.vars.groovy
@@ -1,7 +1,7 @@
 DE_DESeq2_TARGETS="targets=targets.txt"       //targets file. Check the bin directory for the format
 DE_DESeq2_CONTRASTS="contrasts=contrasts.txt" //contrasts file. Check the bin directory for the format
 DE_DESeq2_MMATRIX="mmatrix=~group"        //formula for the linear model
-DE_DESeq2_FILTER="filter=TRUE"                //whether to filter invariant genes
+DE_DESeq2_FILTER="filter=TRUE"                //whether to perform automatic independent filtering of lowly expressed genes
 DE_DESeq2_PREFIX="prefix="                    //prefix to be removed from file names
 DE_DESeq2_SUFFIX="suffix="                    //suffix to be removed from file names
 DE_DESeq2_CWD="cwd=" + RESULTS + "/subread-count"  //directory where the .tsv files with the gene counts are located
