801341eeb622026743c0c0fb1df88a8786f55f95
adomingues <amjdomingues@gmail.com>
2017-06-09 14:04:43 +0200

    changed to include modules - straight copy from modules/NGS/bam2bw

--- modules/SmallRNAseq/bam2bw.module.groovy
+++ modules/SmallRNAseq/bam2bw.module.groovy
@@ -1,32 +1,32 @@
-//rule for task Bam2bw from catalog NGS, version 1
+//rule for task bam2bw from catalog NGS, version 1
 //desc: Create scaled bigwig tracks from a bam file
-Bam2bw = {
-	doc title: "Bam2bw",
+bam2bw = {
+	doc title: "bam2bw",
 		desc:  "Convert BAM file to bigWig",
 		constraints: "none.",
+		bpipe_version: "tested with bpipe 0.9.8.7",
 		author: "Sergi Sayols"
 
 	output.dir=TRACKS
-
+	
 	transform(".bam") to (".scaled.bw") {
 		exec """
+			module load bedtools/${BEDTOOLS_VERSION} &&
+			module load samtools/${SAMTOOLS_VERSION} &&
+			module load kentUtils/${KENTUTILS_VERSION} &&
+
 			if [ ! -d ${TMP} ]; then
 				mkdir -p ${TMP};
 			fi &&
-
-			echo 'VERSION INFO'  1>&2 &&
-         echo \$(${TOOL_BEDTOOLS}/genomeCoverageBed -h 2>&1 | grep 'Version') 1>&2
-			${TOOL_KENTUTILS}/bedGraphToBigWig    1>&2 &&
-			echo '/VERSION INFO' 1>&2 &&
-
+			
 			CHRSIZES=${TMP}/\$(basename ${input.prefix}).bam2bw.chrsizes &&
-			${TOOL_SAMTOOLS}/samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
-			TOTAL_MAPPED=\$( ${TOOL_SAMTOOLS}/samtools flagstat $input | head -n1 | cut -f1 -d" ") &&
+			samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
+			TOTAL_MAPPED=\$( samtools flagstat $input | head -n1| cut -f1 -d" ") &&
 			SCALE=\$(echo "1000000/\$TOTAL_MAPPED" | bc -l) &&
-			${TOOL_BEDTOOLS}/genomeCoverageBed -bg -split -scale \${SCALE} -ibam ${input} -g \${CHRSIZES} > ${output.prefix}.bedgraph &&
-			${TOOL_KENTUTILS}/bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
+			genomeCoverageBed -bg -split -scale \${SCALE} -ibam ${input} -g \${CHRSIZES} > ${output.prefix}.bedgraph &&
+			bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
 			rm \${CHRSIZES} ${output.prefix}.bedgraph
-		""","Bam2bw"
+		""","bam2bw"
 	}
 }
 
