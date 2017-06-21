de1c728305145f0ed7a5e0f4ec8984c0fbefd9e4
ssayols <sergisayolspuig@gmail.com>
2017-06-21 10:50:10 +0200

    add more meaningful value to the universe parameter

--- modules/RNAseq/GO_Enrichment.vars.groovy
+++ modules/RNAseq/GO_Enrichment.vars.groovy
@@ -1,8 +1,8 @@
 GO_Enrichment_RDATA ="rData=" + DE_DESeq2_OUTDIR
 GO_Enrichment_LOG2FOLD="log2Fold=0" 
 GO_Enrichment_PADJ="padj=0.01"
-GO_Enrichment_ORG="organism=mouse"
-GO_Enrichment_UNIV="univ=express"
+GO_Enrichment_ORG="organism=" + ESSENTIAL_ORG
+GO_Enrichment_UNIV="univ=expressed"
 GO_Enrichment_TYPE="type=gene_name" 
 GO_Enrichment_CATEGORY="plotCategory=5"
 GO_Enrichment_OUTDIR="out=" + RESULTS + "/GO_Analysis"
