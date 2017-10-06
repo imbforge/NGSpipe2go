147b9b29f474bc10ddcb105fddea271511442b5a
adomingues <amjdomingues@gmail.com>
2017-10-06 08:26:55 +0200

    Changed location of project folders to be more intuitive. Data contains only raw data, everything else that requires looking into goes to results.

--- modules/SmallRNAseq/essential.vars.groovy
+++ modules/SmallRNAseq/essential.vars.groovy
@@ -32,7 +32,7 @@ ESSENTIAL_MISMATCHES=0
 
 
 // ZEBRAFISH
-ESSENTIAL_PROJECT="."
+/*ESSENTIAL_PROJECT="."
 ESSENTIAL_BOWTIE_REF="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/BowtieIndexChr/chr"
 ESSENTIAL_GENOME_REF="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/chr_sequences/chr.clean.fa"
 // vars for piRNA analyis
@@ -52,16 +52,18 @@ ESSENTIAL_PAIRED="no"
 ESSENTIAL_MISMATCHES=2
 ESSENTIAL_GENOME_SIZE=1400000000 // Taken from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM1077594. Other sizes (see deppTools docs):  mm9: 2,150,570,000; hg19:2,451,960,000; dm3:121,400,000 and ce10:93,260,000
 
+*/
+
 //global vars that will be reused in some global vars
 PROJECT=ESSENTIAL_PROJECT
-LOGS=PROJECT + "/logs"
-QC=PROJECT + "/data/qc"
-REPORTS=PROJECT + "/reports"
 RESULTS=PROJECT + "/results"
+LOGS=PROJECT + "/logs"
+QC=RESULTS + "/qc"
+REPORTS=RESULTS + "/reports"
 MAPPED=RESULTS + "/mapped"
 UNIQUE_MAPPED=MAPPED + "/unique"
 UNMAPPED=MAPPED + "/unmapped"
 FQ=MAPPED + "/bam2fq"
 MULTI_MAPPED=MAPPED + "/multimapped"
 TMP=PROJECT + "/tmp"
-TRACKS=MAPPED + "/tracks"
+TRACKS=RESULTS + "/tracks"
