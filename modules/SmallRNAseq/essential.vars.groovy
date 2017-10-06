e8780939237b269e9abdcca5b68994c1167e3c1b
adomingues <amjdomingues@gmail.com>
2017-10-06 08:07:32 +0200

    zebrafish variables added back, they were omitted by mistake

--- modules/SmallRNAseq/essential.vars.groovy
+++ modules/SmallRNAseq/essential.vars.groovy
@@ -5,7 +5,7 @@
 
 /*COMMENT OUT THE SPECIES TO BE ANALYSED*/
 // C. ELEGANS
-ESSENTIAL_PROJECT="/fsimb/groups/imb-kettinggr/adomingues/projects/imb_ketting_2017_10_sensor_and_mutant"
+ESSENTIAL_PROJECT="."
 ESSENTIAL_BOWTIE_REF="/home/adomingu/imb-kettinggr/genomes/Caenorhabditis_elegans/Ensembl/WBcel235/Sequence/BowtieIndexSensor/genome_sensor"
 ESSENTIAL_GENOME_REF="/home/adomingu/imb-kettinggr/genomes/Caenorhabditis_elegans/Ensembl/WBcel235/Sequence/BowtieIndexSensor/genome_sensor.fa"
 // vars for smRNA analysis
@@ -30,6 +30,27 @@ ESSENTIAL_PAIRED="no"
 ESSENTIAL_THREADS=4
 ESSENTIAL_MISMATCHES=0
 
+
+// ZEBRAFISH
+ESSENTIAL_PROJECT="."
+ESSENTIAL_BOWTIE_REF="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/BowtieIndexChr/chr"
+ESSENTIAL_GENOME_REF="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/chr_sequences/chr.clean.fa"
+// vars for piRNA analyis
+ESSENTIAL_FEATURES="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/pipeline/features.bed"
+// vars for mirDeep2
+ESSENTIAL_MATURE_MIRNA="/fsimb/groups/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/SmallRNA/mature.noSpaces.fa"
+ESSENTIAL_HAIRPIN_MIRNA="~/imb-git addkettinggr/genomes/Danio_rerio/Ensembl/Zv9/Annotation/SmallRNA/hairpin.noSpaces.fa"
+ESSENTIAL_REPEAT_MASKER="/home/adomingu/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/repeatMasker/danRer7.noSimpleLow.fa.out"
+ESSENTIAL_REPEAT_REF="/home/adomingu/imb-kettinggr/genomes/Danio_rerio/Ensembl/Zv9/Sequence/repEnrichNoSimpleLow"
+REPENRICH_BED="FALSE" 
+ESSENTIAL_THREADS=4
+// read size to keep (added 8 bp to account for barcodes)
+MIN_LENGTH=28
+MAX_LENGTH=45
+ESSENTIAL_STRANDED="reverse"
+ESSENTIAL_PAIRED="no"
+ESSENTIAL_MISMATCHES=2
+ESSENTIAL_GENOME_SIZE=1400000000 // Taken from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM1077594. Other sizes (see deppTools docs):  mm9: 2,150,570,000; hg19:2,451,960,000; dm3:121,400,000 and ce10:93,260,000
 
 //global vars that will be reused in some global vars
 PROJECT=ESSENTIAL_PROJECT
