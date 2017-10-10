4cd5524507bc97ea7a7e0bf141662750bcb84203
ssayols <sergisayolspuig@gmail.com>
2017-10-10 09:31:19 +0200

    add parameters to choose the organism for peak annotation

--- modules/ChIPseq/peak_annotation.vars.groovy
+++ modules/ChIPseq/peak_annotation.vars.groovy
@@ -1,7 +1,7 @@
 peak_annotation_FILES="peakData="+ RESULTS+ "/macs2"
 peak_annotation_TRANSCRIPT_TYPE="transcriptType=Bioconductor"
-peak_annotation_TRANSCRIPT_DB="transcriptDb=TxDb.Mmusculus.UCSC.mm9.knownGene"
-peak_annotation_ORGDB="orgDb=org.Mm.eg.db"
+peak_annotation_TRANSCRIPT_DB="transcriptDb=" + ESSENTIAL_TXDB   // eg, TxDb.Mmusculus.UCSC.mm9.knownGene"
+peak_annotation_ORGDB="orgDb=" + ESSENTIAL_ANNODB                // eg, org.Mm.eg.db
 peak_annotation_REGIONTSS="regionTSS=3000"
 peak_annotation_OUTDIR="out="+ RESULTS+ "/Peak_Annotation"
 peak_annotation_EXTRA=""
