10d44581b698703b5bbb5c3712897693f3f3f816
adomingues <amjdomingues@gmail.com>
2017-10-18 14:57:44 +0200

    Fixed indentation/style, and added log files

--- modules/SmallRNAseq/fastq_quality_filter.module.groovy
+++ modules/SmallRNAseq/fastq_quality_filter.module.groovy
@@ -5,16 +5,21 @@ FastQQualityFilter = {
 		author: "Antonio Domingues"
 
 	output.dir = FASTQ_QUALITY_FILTER_OUTDIR
-   FASTQ_QUALITY_FILTER_FLAGS= " -q "   + MIN_QUAL  +
-                " -p "   + MIN_PERCENT  +
-             " -Q " + QUAL_FORMAT +
-             FASTQ_QUALITY_FILTER_OTHER
 
-   transform(".cutadapt.fastq.gz") to (".highQ.fastq.gz") {
-      exec """
-         module load fastx_toolkit/${FASTX_VERSION} &&
+    FASTQ_QUALITY_FILTER_FLAGS = " -q "   + MIN_QUAL  +
+                                 " -p "   + MIN_PERCENT  +
+                                 " -Q " + QUAL_FORMAT +
+                                 FASTQ_QUALITY_FILTER_OTHER
 
-         zcat $input | fastq_quality_filter $FASTQ_QUALITY_FILTER_FLAGS -o $output
+    transform(".cutadapt.fastq.gz") to (".highQ.fastq.gz") {
+
+        def EXP = input.prefix.prefix.prefix
+
+        exec """
+            module load fastx_toolkit/${FASTX_VERSION} &&
+
+            zcat $input | fastq_quality_filter $FASTQ_QUALITY_FILTER_FLAGS -o $output 2>&1 >> ${FASTQ_QUALITY_FILTER_OUTDIR}/${EXP}.fastq_quality_filter.log
+            
       ""","FastQQualityFilter"
    }
 }
