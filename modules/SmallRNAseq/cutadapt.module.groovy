6399cb9201da9e0959a6f6caee7f7664784783e0
adomingues <amjdomingues@gmail.com>
2017-10-18 10:49:41 +0200

    keep discarded, too long, reads

--- modules/SmallRNAseq/cutadapt.module.groovy
+++ modules/SmallRNAseq/cutadapt.module.groovy
@@ -5,13 +5,15 @@ Cutadapt = {
       author: "Antonio Domingues"
 
    output.dir = CUTADAPT_OUTDIR
+   def EXP = input.split("/")[-1].replaceAll(".fastq.gz", "")
 
-   transform(".fastq.gz") to (".cutadapt.fastq.gz") {
+   transform(".fastq.gz") to (".cutadapt.fastq.gz",".cutadapt_discarded.fastq.gz") {
       exec """
 
          module load cutadapt/${CUTADAPT_VERSION} &&
 
-         cutadapt $ADAPTER_SEQUENCE -O $MINIMUM_OVERLAP -m $MINIMUM_LENGTH_KEEP -M $MAXIMUM_LENGTH_KEEP -o $output $input 2>&1 >> ${CUTADAPT_OUTDIR}/cutadapt.log
+         cutadapt $ADAPTER_SEQUENCE -O $MINIMUM_OVERLAP -m $MINIMUM_LENGTH_KEEP -M $MAXIMUM_LENGTH_KEEP -o $output1 --too-long-output $output2 $input 2>&1 >> ${CUTADAPT_OUTDIR}/${EXP}.cutadapt.log
+         
       ""","Cutadapt"
    }
 }
