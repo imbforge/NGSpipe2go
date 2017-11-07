4f9bb4d25b29f3632b225e2812292520654bacbd
adomingues <amjdomingues@gmail.com>
2017-11-07 09:13:04 +0100

    Fixed sample name and output directory for the log

--- modules/SmallRNAseq/fastq_quality_filter.module.groovy
+++ modules/SmallRNAseq/fastq_quality_filter.module.groovy
@@ -1,10 +1,11 @@
 FastQQualityFilter = {
-	doc title: "Remove sequences",
-		desc:  "filter reads containing low-quality (Phred score below 20) bases in order to facilitate the PCR duplicates removal.",
-		constraints: "Only supports compressed FASTQ files",
-		author: "Antonio Domingues"
+    doc title: "Remove sequences",
+        desc:  "filter reads containing low-quality (Phred score below 20) bases in order to facilitate the PCR duplicates removal.",
+        constraints: "Only supports compressed FASTQ files",
+        author: "Antonio Domingues"
 
-	output.dir = FASTQ_QUALITY_FILTER_OUTDIR
+    output.dir = FASTQ_QUALITY_FILTER_OUTDIR
+    def EXP = input.split("/")[-1].replaceAll(".cutadapt.fastq.gz", "")
 
     FASTQ_QUALITY_FILTER_FLAGS = " -q "   + MIN_QUAL  +
                                  " -p "   + MIN_PERCENT  +
@@ -13,7 +14,6 @@ FastQQualityFilter = {
 
     transform(".cutadapt.fastq.gz") to (".highQ.fastq.gz") {
 
-        def EXP = input.prefix.prefix.prefix
 
         exec """
             module load fastx_toolkit/${FASTX_VERSION} &&
