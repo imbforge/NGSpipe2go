af7798041e854c2966c4c17761141316caa0ebf2
GPetrosino <gius.petrosino@gmail.com>
2018-04-06 17:39:27 +0200

    Remove blacklist var

--- modules/NGS/bamcoverage.vars.groovy
+++ modules/NGS/bamcoverage.vars.groovy
@@ -2,4 +2,3 @@ BAMCOVERAGE_CORES="--numberOfProcessors " + Integer.toString(ESSENTIAL_THREADS)
 BAMCOVERAGE_OTHER="--smoothLength 60 --binSize 20 --normalizeUsingRPKM --outFileFormat bigwig" //if you want to exclude chromsomes for normalisation e.g. rDNA or mitochondrion add the following parameter --ignoreForNormalization \"chrM, rDNA\", if you like to use offsets, blacklist regions, center reads or anything like it please refer to the deepTools manual, there is even a special modus for Nucleosome detection in Mnase data 
 BAMCOVERAGE_OUTDIR=TRACKS
 BAMCOVERAGE_FRAGMENTS=ESSENTIAL_FRAGMENT_USAGE
-BAMCOVERAGE_BR="--blackListFileName" + ESSENTIAL_BLACKLIST // A BED or GTF file containing regions that should be excluded from all analyses.
