bb6febdb44f7f4e50379e7506f651797d17fc2f1
karaulanov <karaulanov@users.noreply.github.com>
2018-08-09 17:37:32 +0200

    optimise parameters and skip default ones

--- modules/RNAseq/star.vars.groovy
+++ modules/RNAseq/star.vars.groovy
@@ -1,21 +1,14 @@
 // local settings
-STAR_MAXRAM = "--limitGenomeGenerateRAM 31000000000"// around 30Gb for mammals
-STAR_BUFSIZE = "--limitIObufferSize 150000000"	    // buffer size
-STAR_MM = "--outFilterMismatchNmax 2"               // number of mismatches allowed
-STAR_MULTIMAP = "--outFilterMultimapNmax 10"        // max multimap positions per read
-STAR_MININTRO = "--alignIntronMin 21"               // minimum intron size
-STAR_UNMAPPED_BAM = "--outSAMunmapped Within "      // report unmapped reads to bam file? (choices: Within, None)
-STAR_UNMAPPED_OUT = "--outReadsUnmapped None"       // report unmapped reads to separate fasta/fastq files, besides --outSAMunmapped? (choices: Fastx, None)
-
+STAR_MM = "--outFilterMismatchNoverLmax 0.04"		// max. percentage of mismatches in the alignment (4% used by ENCODE)
+STAR_UNMAPPED_BAM = "--outSAMunmapped Within"		// report unmapped reads to bam file? (choices: Within, None)
 
 // settings imported from essential vars
 STAR_THREADS = "--runThreadN " + Integer.toString(ESSENTIAL_THREADS)
 STAR_REF = "--genomeDir " + ESSENTIAL_STAR_REF
 STAR_OVERHANG = "--sjdbOverhang " + Integer.toString(ESSENTIAL_READLENGTH - 1)
-STAR_GTF = "--sjdbGTFfile " + ESSENTIAL_GENESGTF    // gene model
+STAR_GTF = "--sjdbGTFfile " + ESSENTIAL_GENESGTF		// gene model GTF file
 
-STAR_FILTER_SEC="YES"		// filter out secondary alignments from the bam file?
-STAR_EXTRA=""               // extra parms to sent to the tool
-
-// samtools section
+// additional settings
+STAR_EXTRA=""		// extra parms to sent to the tool
+STAR_FILTER_SEC="YES"		// remove secondary alignments in order to keep just 1 alignment of the multi-mapping reads ?
 STAR_SAMTOOLS_THREADS= "-@ " + Integer.toString(ESSENTIAL_THREADS)
