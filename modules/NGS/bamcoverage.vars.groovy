234582efd8c5f3913886234af9ae2e4e74588c3e
nkreim <n.kreim@imb-mainz.de>
2018-10-30 14:54:41 +0100

    in deeptools v3 the arguemnt --normalizeusingRPKM was replaced by --normlizeusing RPKM. This should be compatible with deeptools v3.

--- modules/NGS/bamcoverage.vars.groovy
+++ modules/NGS/bamcoverage.vars.groovy
@@ -1,4 +1,4 @@
 BAMCOVERAGE_CORES="--numberOfProcessors " + Integer.toString(ESSENTIAL_THREADS)
-BAMCOVERAGE_OTHER="--smoothLength 60 --binSize 20 --normalizeUsingRPKM --outFileFormat bigwig" //if you want to exclude chromsomes for normalisation e.g. rDNA or mitochondrion add the following parameter --ignoreForNormalization \"chrM, rDNA\", if you like to use offsets, blacklist regions, center reads or anything like it please refer to the deepTools manual, there is even a special modus for Nucleosome detection in Mnase data 
+BAMCOVERAGE_OTHER="--smoothLength 60 --binSize 20 --normalizeUsing RPKM --outFileFormat bigwig" //if you want to exclude chromsomes for normalisation e.g. rDNA or mitochondrion add the following parameter --ignoreForNormalization \"chrM, rDNA\", if you like to use offsets, blacklist regions, center reads or anything like it please refer to the deepTools manual, there is even a special modus for Nucleosome detection in Mnase data 
 BAMCOVERAGE_OUTDIR=TRACKS
 BAMCOVERAGE_FRAGMENTS=ESSENTIAL_FRAGMENT_USAGE
