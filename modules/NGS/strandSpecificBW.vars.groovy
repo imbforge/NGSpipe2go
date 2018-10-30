953feed5a9c781ae1be43cc59fb0b8b004bd54b9
nkreim <n.kreim@imb-mainz.de>
2018-10-30 15:03:36 +0100

    Modification of vars for deeptools v3 API adjustment.

--- modules/NGS/strandSpecificBW.vars.groovy
+++ modules/NGS/strandSpecificBW.vars.groovy
@@ -1,4 +1,4 @@
 //VARS for task strandspecific
 STRANDSPECIFICBIGWIG_CORES=4
-STRANDSPECIFICBIGWIG_OTHER="--smoothLength 60 --binSize 20 --normalizeUsingRPKM --skipNonCoveredRegions --outFileFormat bedgraph"
+STRANDSPECIFICBIGWIG_OTHER="--smoothLength 60 --binSize 20 --normalizeUsing RPKM --skipNonCoveredRegions --outFileFormat bedgraph"
 STRANDSPECIFICBIGWIG_OUTDIR=TRACKS + "/strandspecific"
