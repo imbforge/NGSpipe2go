f0399d663fa17a345d7b155b7d47dd1ed504f278
nkreim <n.kreim@imb-mainz.de>
2018-10-30 15:21:02 +0100

    Added a comment regarding an API change of deeptools, changed the debian9 versions of tools for RNA-seq

--- modules/NGS/strandSpecificBW.vars.groovy
+++ modules/NGS/strandSpecificBW.vars.groovy
@@ -1,4 +1,4 @@
 //VARS for task strandspecific
 STRANDSPECIFICBIGWIG_CORES=4
-STRANDSPECIFICBIGWIG_OTHER="--smoothLength 60 --binSize 20 --normalizeUsing RPKM --skipNonCoveredRegions --outFileFormat bedgraph"
+STRANDSPECIFICBIGWIG_OTHER="--smoothLength 60 --binSize 20 --normalizeUsingRPKM --skipNonCoveredRegions --outFileFormat bedgraph" // if you use > v3 modify the vars to --normalizeUsing RPKM since the API of deeptools changed
 STRANDSPECIFICBIGWIG_OUTDIR=TRACKS + "/strandspecific"
