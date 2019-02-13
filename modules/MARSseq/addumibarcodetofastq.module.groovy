1db727b13ecf3e29aa25dbbb8a9817576ffd987b
nkreim <n.kreim@imb-mainz.de>
2019-02-13 15:24:32 +0100

    Module to add UMI and cellbarcode of MARS-Seq data to the fastq header

---
+++ modules/MARSseq/addumibarcodetofastq.module.groovy
@@ -0,0 +1,27 @@
+AddUMIBarcodeToFastq = {
+	doc title: "Adds UMI and Barcode of to the fastq header",
+		desc:  "adds UMI and barcode of the second read in MARS-Seq samples to the fastq header using umitools",
+		constraints: "",
+		bpipe_version: "tested with bpipe 0.9.9.2",
+		author: "Nastasja Kreim"
+
+		output.dir = ADDUMIBARCODE_OUTDIR
+    def OUTPUTFILE = input1
+    int path_index = OUTPUTFILE.lastIndexOf("/")
+    OUTPUTFILE = OUTPUTFILE.substring(path_index+1)
+    OUTPUTFILE = (OUTPUTFILE =~ /.R1.fastq.gz/).replaceFirst("")
+    ADDUMIBARCODE_FLAGS =        ADDUMIBARCODE_BCPATTERN + 
+                                  " " +  ADDUMIBARCODE_BARCODELIST +
+                                  " " +  ADDUMIBARCODE_EXTRA
+
+
+
+  produce(OUTPUTFILE + ".umibarcode.fastq.gz"){
+    exec """
+     module load umitools/${UMITOOLS_VERSION} &&
+     umi_tools extract $ADDUMIBARCODE_FLAGS -I $input2 --stdout ${input2.prefix}.barcode.fastq.gz --read2-in $input1 --read2-out=$output &&
+     rm ${input2.prefix}.barcode.fastq.gz
+  ""","AddUMIBarcodeToFastq"
+}
+}
+
