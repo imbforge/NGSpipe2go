255307cdae9ea77c08f1f6f61b1cd657ec955efe
nkreim <n.kreim@imb-mainz.de>
2019-02-13 15:35:20 +0100

    Tool versions and location for MARS-Seq pipeline

---
+++ modules/MARSseq/tool.locations.groovy
@@ -0,0 +1,3 @@
+// variables containing the location of the used tools
+PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/NGSpipe2go/tools" // please copy the bpipe tools to the project folder and include the location here
+TOOL_COLLECT=PROJECT_DEPENDENCIES + "/collectBpipeLogs"
