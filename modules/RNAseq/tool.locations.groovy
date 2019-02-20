3b46ed82e26e049cb8c8a5932bc7cac3c2ae181a
GPetrosino <gius.petrosino@gmail.com>
2019-02-20 18:01:33 +0100

    Update tool.locations.groovy

--- modules/RNAseq/tool.locations.groovy
+++ modules/RNAseq/tool.locations.groovy
@@ -2,8 +2,8 @@
 TOOL_DEPENDENCIES="/fsimb/common/tools"
 PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/NGSpipe2go/tools" // please copy the bpipe tools to the project folder and include the location here
 TOOL_DUPRADAR=PROJECT_DEPENDENCIES + "/dupRadar"
-TOOL_RNAtypes=TOOL_DEPENDENCIES + "/imb-forge/RNAtypes"
-TOOL_GENEBODYCOV2=TOOL_DEPENDENCIES + "/imb-forge/geneBodyCov"
+TOOL_RNAtypes=PROJECT_DEPENDENCIES + "/RNAtypes"
+TOOL_GENEBODYCOV2=PROJECT_DEPENDENCIES + "/geneBodyCov"
 TOOL_EDGER=PROJECT_DEPENDENCIES + "/DE_edgeR"
 TOOL_DESeq2=PROJECT_DEPENDENCIES + "/DE_DESeq2"
 TOOL_COLLECT=PROJECT_DEPENDENCIES + "/collectBpipeLogs"
