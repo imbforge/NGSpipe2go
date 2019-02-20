694034b1505cd93f621d48af476132d92107977d
GPetrosino <gius.petrosino@gmail.com>
2019-02-20 17:53:41 +0100

    Update tool.locations.groovy

--- modules/ChIPseq/tool.locations.groovy
+++ modules/ChIPseq/tool.locations.groovy
@@ -1,7 +1,7 @@
 // variables containing the location of the used tools
 TOOL_DEPENDENCIES="/fsimb/common/tools" // your local tools folder
 PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/NGSpipe2go/tools" // please copy the NGSpipe2Go tools to the project folder and include the location here
-TOOL_ENCODEqc=TOOL_DEPENDENCIES + "/imb-forge/chipqc"
+TOOL_ENCODEqc=PROJECT_DEPENDENCIES + "/ENCODEqc"
 TOOL_COLLECT=PROJECT_DEPENDENCIES + "/collectBpipeLogs"
 TOOL_DIFFBIND=PROJECT_DEPENDENCIES + "/diffbind"
 TOOL_PEAK_ANNOTATION=PROJECT_DEPENDENCIES + "/Peak_Annotation"
