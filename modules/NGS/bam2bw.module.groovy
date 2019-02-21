3c3c9c3308d044c4f7675b8042c992bd8dd95a62
GPetrosino <gius.petrosino@gmail.com>
2019-02-21 19:50:16 +0100

    Update bam2bw.module.groovy

--- modules/NGS/bam2bw.module.groovy
+++ modules/NGS/bam2bw.module.groovy
@@ -23,8 +23,7 @@ bam2bw = {
 			samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
 			TOTAL_MAPPED=\$( samtools flagstat $input | head -n1| cut -f1 -d" ") &&
 			SCALE=\$(echo "1000000/\$TOTAL_MAPPED" | bc -l) &&
-			genomeCoverageBed -bg -split -scale \${SCALE} -ibam ${input} -g \${CHRSIZES} > ${output.prefix}.bedgraph &&
-			bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
+                        genomeCoverageBed -bg -split -scale \${SCALE} -ibam ${input} | sortBed -i -  > ${output.prefix}.bedgraph &&			bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
 			rm \${CHRSIZES} ${output.prefix}.bedgraph
 		""","bam2bw"
 	}
