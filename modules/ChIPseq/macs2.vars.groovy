928e62e58fbdb241ed5789eb312e2fc2b9da2b97
karaulanov <karaulanov@users.noreply.github.com>
2019-03-22 12:16:36 +0100

    Update macs2.vars.groovy

--- modules/ChIPseq/macs2.vars.groovy
+++ modules/ChIPseq/macs2.vars.groovy
@@ -1,10 +1,8 @@
-//vars for task macs2 from catalog ChIPseq, version 1
+// vars for task macs2 from catalog ChIPseq
 MACS2_TARGETS="targets.txt" // targets file describing the samples
-MACS2_MFOLD="-m 5 50"	// range of enrichment ratio (default: 5,50)
 MACS2_GSIZE="-g " + ESSENTIAL_MACS2_GSIZE // the mappable genome size
-MACS2_BWIDTH="--bw " + Integer.toString(ESSENTIAL_FRAGLEN)	  // bandwidth use for model building
+MACS2_BWIDTH="--bw " + Integer.toString(ESSENTIAL_FRAGLEN) // bandwidth for model building with SR data
+MACS2_MINLEN="--min-length " + Integer.toString(ESSENTIAL_FRAGLEN) // minimum peak length
 MACS2_MAPPED=MAPPED // where the bam files are stored
-//keep duplicates based on the binominal distribution
-MACS2_EXTRA="--keep-dup " + ESSENTIAL_DUP		// other parms sent to macs2
-MACS2_PAIRED=ESSENTIAL_PAIRED // are we dealing with paired end data -> use fragments in MACS2
-
+MACS2_EXTRA="--keep-dup " + ESSENTIAL_DUP		// other parameters sent to macs2
+MACS2_PAIRED=ESSENTIAL_PAIRED // for PE data use fragments in peak calling
