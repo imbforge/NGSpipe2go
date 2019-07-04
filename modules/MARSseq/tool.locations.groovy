bbf883f1af13cf123ac4cfbb2794a12843ce7248
Frank Rühle <f.ruehle@imb-mainz.de>
2019-07-04 14:47:34 +0200

    include dupRadar
    add dupRadar in Mars-Seq tool.locations.groovy

--- modules/MARSseq/tool.locations.groovy
+++ modules/MARSseq/tool.locations.groovy
@@ -1,6 +1,7 @@
 // variables containing the location of the used tools
 TOOL_DEPENDENCIES="/fsimb/common/tools"
 PROJECT_DEPENDENCIES=ESSENTIAL_PROJECT + "/NGSpipe2go/tools" // please copy the bpipe tools to the project folder and include the location here
+TOOL_DUPRADAR=PROJECT_DEPENDENCIES + "/dupRadar"
 TOOL_COLLECT=PROJECT_DEPENDENCIES + "/collectBpipeLogs"
 TOOL_RNAtypes=PROJECT_DEPENDENCIES + "/RNAtypes"
 TOOL_GENEBODYCOV2=PROJECT_DEPENDENCIES + "/geneBodyCov"
