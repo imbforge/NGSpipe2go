a966fd770c6f87afc755ee13026e0e693a96a63c
ssayols <sergisayolspuig@gmail.com>
2019-10-25 13:35:44 +0200

    take threads from essential vars

--- modules/RNAseqVariantCalling/create_star_index_sjdb.vars.groovy
+++ modules/RNAseqVariantCalling/create_star_index_sjdb.vars.groovy
@@ -2,7 +2,7 @@ GenerateStarIndexFromSJ_vars=[
     outdir    : MAPPED + "/sjdbStarIndex",
     sjdbfile  : MAPPED + "/sjdbStarIndex/SJ.out.tab.Pass1.sjdb",
     outdir_2nd_index: FilterAndMergeSJtab_vars.outdir,
-    threads   : STAR_pe_vars.threads,
+    threads   : Integer.toString(ESSENTIAL_THREADS),
     genome_ref: ESSENTIAL_GENOME_REF,
     maxram    : STAR_pe_vars.maxram,
     bufsize   : STAR_pe_vars.bufsize,
