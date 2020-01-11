b4903c102c73fb732250b7f4789561ece352cb90
ssayols <sergisayolspuig@gmail.com>
2020-01-11 13:42:19 +0100

    create output directory if it doesnt exists

--- modules/miscellaneous/collect_tool_versions.module.groovy
+++ modules/miscellaneous/collect_tool_versions.module.groovy
@@ -10,8 +10,12 @@ collectToolVersions = {
 		bpipe_version: "tested with bpipe 0.9.9.8",
 		author: "Sergi Sayols"
 	
-    File f = new File(collectToolVersions_vars.outdir + "/tool_versions.txt")
-    f.write "tool\tenv\tversion\n"
-    tools.each { tool, x -> f << "$tool\t$x.runenv\t$x.version\n" }
+	output.dir = collectToolVersions_vars.outdir
+
+    produce("tool_versions.txt") {
+        File f = new File(collectToolVersions_vars.outdir + "/tool_versions.txt")
+        f.write "tool\tenv\tversion\n"
+        tools.each { tool, x -> f << "$tool\t$x.runenv\t$x.version\n" }
+    }
 }
 
