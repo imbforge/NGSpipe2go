84acdaec100066bd053a2601806cc9812be3a4cb
ssayols <sergisayolspuig@gmail.com>
2020-02-26 13:24:41 +0100

    use gatk old style command line args

--- modules/DNAseq/realignment.module.groovy
+++ modules/DNAseq/realignment.module.groovy
@@ -15,11 +15,11 @@ IndelRealignment = {
     def RealignerTargetCreator_FLAGS =
         (IndelRealignment_vars.threads        ? " -nt "     + IndelRealignment_vars.threads : "" ) +
         (IndelRealignment_vars.bwa_ref        ? " -R "      + IndelRealignment_vars.bwa_ref : "" ) +
-        (IndelRealignment_vars.mills_variants ? " --known " + IndelRealignment_vars.mills_variants : "" )
+        (IndelRealignment_vars.mills_variants ? " -known " + IndelRealignment_vars.mills_variants : "" )
 
     def IndelRealignment_FLAGS =
         (IndelRealignment_vars.bwa_ref        ? " -R "      + IndelRealignment_vars.bwa_ref : "" ) +
-        (IndelRealignment_vars.mills_variants ? " --known " + IndelRealignment_vars.mills_variants : "" )
+        (IndelRealignment_vars.mills_variants ? " -known " + IndelRealignment_vars.mills_variants : "" )
 
     def TOOL_ENV = prepare_tool_env("java", tools["java"]["version"], tools["java"]["runenv"]) + " && " +
                    prepare_tool_env("gatk", tools["gatk"]["version"], tools["gatk"]["runenv"])
