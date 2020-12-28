3de4cb89940b92affba33f054cf9137493ae54e4
Antonio Domingues <amjdomingues@gmail.com>
2020-12-28 22:36:38 +0100

    Bug fix. With sensor as a contig the file needs to be sorted

--- modules/SmallRNAseq/bam2bw.module.groovy
+++ modules/SmallRNAseq/bam2bw.module.groovy
@@ -1,40 +1,35 @@
-load MODULE_FOLDER + "SmallRNAseq/bam2bw.vars.groovy"
-
+//rule for task Bam2bw from catalog NGS, version 1
+//desc: Create scaled bigwig tracks from a bam file
 Bam2bw = {
-    doc title: "Bam2bw",
-        desc:  "Convert BAM file to bigWig",
-        constraints: "none.",
-        author: "Sergi Sayols, Antonio Domingues"
+	doc title: "Bam2bw",
+		desc:  "Convert BAM file to bigWig",
+		constraints: "none.",
+		author: "Sergi Sayols, Antonio Domingues"
 
-    output.dir=TRACKS
+	output.dir=TRACKS
+	
+	def SAMPLE_NAME = input.split("/")[-1].split("\\.", 2)[0]
 
-    def SAMPLE_NAME = input.split("/")[-1].split("\\.", 2)[0]
+	transform(".bam") to (".scaled.bw") {
+		exec """
+			module load bedtools/${BEDTOOLS_VERSION} &&
+			module load samtools/${SAMTOOLS_VERSION} &&
+			module load kentUtils/${KENTUTILS_VERSION} &&
 
-    transform(".bam") to (".scaled.bw") {
-        exec """
-            module load bedtools/${BEDTOOLS_VERSION} &&
-            module load samtools/${SAMTOOLS_VERSION} &&
-            module load kentUtils/${KENTUTILS_VERSION} &&
+			if [ ! -d ${TMP} ]; then
+				mkdir -p ${TMP};
+			fi &&
+			
+			CHRSIZES=${TMP}/\$(basename ${input.prefix}).bam2bw.chrsizes &&
+			samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
+			
+			TOTAL_MAPPED=\$( samtools flagstat $input | head -n5 | tail -n1 | cut -f1 -d" ") &&
 
-            if [ ! -d ${TMP} ]; then
-                mkdir -p ${TMP};
-            fi &&
-
-            if [ "${NORMALIZATION_TO_NONSTRUCT}" == "yes" ];
-            then
-                TOTAL_MAPPED=\$(grep $SAMPLE_NAME ${output.dir}/normalization_factors.txt | cut -f 2);
-            else
-                TOTAL_MAPPED=\$( samtools flagstat $input | head -n1| cut -f1 -d" ");
-            fi &&
-
-            CHRSIZES=${TMP}/\$(basename ${input.prefix}).bam2bw.chrsizes &&
-            samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
-
-            SCALE=\$(echo "1000000/\$TOTAL_MAPPED" | bc -l) &&
-            genomeCoverageBed -bg -split -scale \${SCALE} -ibam ${input} -g \${CHRSIZES} > ${output.prefix}.bedgraph &&
-            bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
-            rm \${CHRSIZES} ${output.prefix}.bedgraph
-        ""","Bam2bw"
-    }
+			SCALE=\$(echo "1000000/\$TOTAL_MAPPED" | bc -l) &&
+			genomeCoverageBed -bg -split -scale -ibam ${input} -g \${CHRSIZES} | bedSort stdin stdout > ${output.prefix}.bedgraph &&
+			bedGraphToBigWig ${output.prefix}.bedgraph \${CHRSIZES} $output &&
+			rm \${CHRSIZES} ${output.prefix}.bedgraph
+		""","Bam2bw"
+	}
 }
 
