bd56191ba144e7a92aa416f3023f188bdc800d5c
Antonio Domingues <amjdomingues@gmail.com>
2021-01-04 00:14:41 +0100

    Added new modules
    - The BCF modules would need to be optimized for my output
    - In the future mapping could be done with BCF bowtie module

---
+++ modules/SmallRNAseq/mapping_stats.module.groovy
@@ -0,0 +1,19 @@
+MappingStats = {
+	doc title: "Statistics of mapping efficiency",
+		desc:  "Counts the number of reads in the mapped bam, including total, unique, and mapped. Returns a plot of the results.",
+      constraints: "Bam files produced by Bowtie 1.x. Might not work for other mappers/versions.",
+		author: "Antonio Domingues"
+
+	produce(
+        MAPPING_STATS_OUTDIR + "/totalReads.pdf",
+        MAPPING_STATS_OUTDIR + "/totalReads.png") {
+
+      exec """
+         module load R/${R_VERSION} &&
+
+         cd ${MAPPING_STATS_OUTDIR} &&
+		 Rscript ${MAPPING_STATS_TOOL}
+
+		""","MappingStatsPlot"
+	}
+}
