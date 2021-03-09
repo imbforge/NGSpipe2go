81deb192454a77cd9ee2a4b27da15f6c0dfe520e
frankRuehle <f.ruehle@imb-mainz.de>
2021-03-09 12:58:07 +0100

    include variable for cutadapt stats in report module

--- modules/NGS/cutadapt.groovy
+++ modules/NGS/cutadapt.groovy
@@ -1,25 +1,22 @@
 Cutadapt = {
         doc title: "Cutadapt",
         desc:  "remove adapter from reads for single end and paired end design",
-        constraints: "Only supports compressed FASTQ files",
+        constraints: "Only supports compressed FASTQ files. Required naming scheme is .fastq.gz for se and .R[12].fastq.gz for pe design.",
         author: "Antonio Domingues, Anke Busch, Nastasja Kreim, Frank Rühle"
 
     output.dir = Cutadapt_vars.outdir
 
     def CUTADAPT_PAIRED = Cutadapt_vars.paired
 
-    def File SAMPLENAME = new File(input.prefix.prefix)
+    def SAMPLENAME = new File(input.prefix.prefix)
     def SAMPLENAME_BASE = SAMPLENAME.getName()
-    if (Cutadapt_vars.paired == true) { // optional read2 in paired end design
-       def File SAMPLENAME2 = new File(input2.optional.prefix.prefix)
-       def SAMPLENAME_BASE2 = SAMPLENAME2.getName()
-    }
-
+    def SAMPLENAME_BASE_PRUNED = SAMPLENAME_BASE.replace(".R1", "") // delete .R1 in combined log file of pe design
+    def SAMPLENAME_BASE_R2 = SAMPLENAME_BASE.replace(".R1", ".R2") // needed for paired end only
 
     // create the log folder if it doesn't exists
-    def CUTADAPT_LOGDIR = new File( Cutadapt_vars.logdir)
-    if (!CUTADAPT_LOGDIR.exists()) {
-        CUTADAPT_LOGDIR.mkdirs()
+    def CUTADAPT_STATSDIR = new File( Cutadapt_vars.statsdir)
+    if (!CUTADAPT_STATSDIR.exists()) {
+        CUTADAPT_STATSDIR.mkdirs()
     }
     // create the discarded folder if it doesn't exists
     def CUTADAPT_DISCARDED_DIR = new File( Cutadapt_vars.outdir + "/discarded")
@@ -29,15 +26,14 @@ Cutadapt = {
 
     def CUTADAPT_FLAGS =
         (Cutadapt_vars.adapter_sequence    ? " --adapter "        + Cutadapt_vars.adapter_sequence    : "") +
-        (Cutadapt_vars.polya               ?                        Cutadapt_vars.polya               : "") +
         (Cutadapt_vars.minimum_overlap     ? " --overlap="        + Cutadapt_vars.minimum_overlap     : "") +
         (Cutadapt_vars.minimum_length_keep ? " --minimum-length " + Cutadapt_vars.minimum_length_keep : "") +
         (Cutadapt_vars.errorrate           ? " --error-rate "     + Cutadapt_vars.errorrate           : "") +
         (Cutadapt_vars.extra               ? " "                  + Cutadapt_vars.extra               : "")
 
     def CUTADAPT_FLAGS_PAIRED = 
-        (Cutadapt_vars.paired ?  " --too-short-paired-output \${TMP}/${SAMPLENAME_BASE2}.cutadapt_discarded.fastq.gz" +
-                                 " -p \${TMP}/${SAMPLENAME_BASE2}.cutadapt.fastq.gz" : "")
+        (Cutadapt_vars.paired ?  " --too-short-paired-output \${TMP}/${SAMPLENAME_BASE_R2}.cutadapt_discarded.fastq.gz" +
+                                 " -p \${TMP}/${SAMPLENAME_BASE_R2}.cutadapt.fastq.gz" : "")
 
     def CUTADAPT_INPUT_SECOND = (Cutadapt_vars.paired ?  " " + input2.optional : "")
 
@@ -50,14 +46,10 @@ Cutadapt = {
             ${TOOL_ENV} &&
             ${PREAMBLE} &&
 
-            cutadapt $CUTADAPT_FLAGS $CUTADAPT_FLAGS_PAIRED --too-short-output=\${TMP}/${SAMPLENAME_BASE}.cutadapt_discarded.fastq.gz --output=\${TMP}/${SAMPLENAME_BASE}.cutadapt.fastq.gz $input1 $CUTADAPT_INPUT_SECOND 2>&1 >> ${CUTADAPT_LOGDIR}/${SAMPLENAME_BASE}.cutadapt.log &&
+            cutadapt $CUTADAPT_FLAGS $CUTADAPT_FLAGS_PAIRED --too-short-output=\${TMP}/${SAMPLENAME_BASE}.cutadapt_discarded.fastq.gz --output=\${TMP}/${SAMPLENAME_BASE}.cutadapt.fastq.gz $input1 $CUTADAPT_INPUT_SECOND 1> ${CUTADAPT_STATSDIR}/${SAMPLENAME_BASE_PRUNED}.cutadapt.log &&
 		
-            mv -t ${CUTADAPT_DISCARDED_DIR}/ \${TMP}/*.cutadapt_discarded.fastq.gz &&
-            mv \${TMP}/${SAMPLENAME_BASE}.cutadapt.fastq.gz $output &&
-            if [ $CUTADAPT_PAIRED = true ]; then
-                mv \${TMP}/${SAMPLENAME_BASE2}.cutadapt.fastq.gz $output2;
-            fi 
-
+            mv -t ${CUTADAPT_DISCARDED_DIR}/ \${TMP}/${SAMPLENAME_BASE_PRUNED}*cutadapt_discarded.fastq.gz &&
+            mv -t $output.dir \${TMP}/${SAMPLENAME_BASE_PRUNED}*cutadapt.fastq.gz
         ""","Cutadapt"
     }
 }
