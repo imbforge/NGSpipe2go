9dfe09a02c80fd3da341fea167f7377818eca722
frankRuehle <f.ruehle@imb-mainz.de>
2021-03-17 16:19:23 +0000

    add ESSENTIAL_FRAGLEN to RNA-Seq essential vars because needed for bamCoverage module
    (cherry picked from commit ddf224ba6bb8d62b92210a49998b2cd361106e0b)

--- pipelines/RNAseq/essential.vars.groovy
+++ pipelines/RNAseq/essential.vars.groovy
@@ -11,8 +11,9 @@ ESSENTIAL_ORG="mouse"           // UCSC organism
 ESSENTIAL_DB="mm9"              // UCSC assembly version
 ESSENTIAL_READLENGTH=50         // added for STAR version > 2.4.1a
 ESSENTIAL_THREADS=4             // number of threads for parallel tasks
-ESSENTIAL_FRAGMENT_USAGE="no"   //should fragments be reconstituted? should always be no for rnaseq
-ESSENTIAL_FILTER_CHR=""         //chromosomes to include in post-mapping analysis.
+ESSENTIAL_FRAGMENT_USAGE="no"   // should fragments be reconstituted? should always be no for rnaseq
+ESSENTIAL_FRAGLEN=200           // mean length of library inserts (default 200)
+ESSENTIAL_FILTER_CHR=""         // chromosomes to include in post-mapping analysis.
 ESSENTIAL_BAMCOVERAGE="--binSize 1 --skipNonCoveredRegions --normalizeUsing CPM" // NO smoothing should be done for RNAseq
 // size of bins (in bases)  and method to normalize the number of reads per bin to generate bigwig file.
 
