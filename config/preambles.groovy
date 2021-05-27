c72f8d6401200282c215b0cef45772e659810d32
Sergi Sayols Puig <sergisayolspuig@gmail.com>
2021-05-27 14:51:44 +0200

    new branch local logs

--- config/preambles.groovy
+++ config/preambles.groovy
@@ -7,10 +7,21 @@
 //   * modules can have custom preambles defined in the module_preambles Map. The
 //     custom preable _overrides_ the default (unless it is defined as `default_preamble + custom_preamble`).
 // See example in the next paragraph for more details
+//
+// Preambles can have placeholder variables, named within double brackets(eg. {{input}})
+// placeholder variables get replaced at runtime with the actual value. The
+// replacement is done in `get_preamble()`
 default_preamble="""
     export TMP="$TMP";
     [[ -n \$TMP && ! -d \$TMP ]] && mkdir -p "\$TMP";
-    [[ -n \$SLURM_JOB_ID ]] && export TMP="/jobdir/\$SLURM_JOB_ID"
+    [[ -n \$SLURM_JOB_ID ]] && export TMP="/jobdir/\$SLURM_JOB_ID";
+
+    export LOGS="${LOGS}/__module__";
+    [[ -n \$LOGS && ! -d \$LOGS ]] && mkdir -p "\$LOGS";
+    readonly LOG_FILE=\${LOGS}/__input__
+    touch \$LOG_FILE;
+    exec 1>\$LOG_FILE;
+    exec 2>&1
 """
 
 // This map defines the default and module-specific preambles.
@@ -26,9 +37,22 @@ module_preambles=[
 
 //
 // get_preamble
-// Given a module, return its preamble string if exists, the default otherwise.
+// Args: the function receives a Map, with at least 2 elements:
+//   module: the module that is calling the function
+//   input: the input file for which the module is running. This is pick from the runtime
+// Other args are optional, and will be used to replace the placeholder variables
+// used within the preamble.
 //
-String get_preamble (String module) {
-    return (module_preambles.containsKey(module) ? module_preambles[module] : module_preambles.default)
+String get_preamble(Map args) {
+
+  // fetch the preamble for the module, if this exists. Otherwise, pick the default preamble
+  def x = (module_preambles.containsKey(args.module) ? module_preambles[args.module] : module_preambles.default)
+
+  // replace placeholder variables within the preamble with the actual runtime value
+  args.each { key, value ->
+    x = x.replaceAll("__" + key + "__", value)
+  }
+
+  return(x)
 }
 
---
+++ pipelines/test/test.pipeline.groovy
@@ -0,0 +1,32 @@
+PIPELINE="test"
+PIPELINE_VERSION="1.0"
+PIPELINE_ROOT="./NGSpipe2go/"    // may need adjustment for some projects
+
+// essential vars
+PROJECT="/fsimb/imbc_home/ssayolsp/tmp/test"
+LOGS=PROJECT + "/logs"
+OUT=PROJECT + "/out"
+TMP=PROJECT + "/tmp"
+
+// load external things
+load PIPELINE_ROOT + "/config/preambles.groovy"
+load PIPELINE_ROOT + "/config/bpipe.config.groovy"
+
+//MAIN PIPELINE TASK
+test = { 
+  output.dir = OUT
+
+  def PREAMBLE = get_preamble(module:"test", input:branch)
+
+  transform("*.in") to (".out") {
+      exec """
+          >&2 echo "before preamble, logs go to the screen";
+          ${PREAMBLE} &&
+          >&2 echo "after preamble, logs go to the corresponding file";
+          cat $input > $output;
+      """
+  }
+}
+
+Bpipe.run { "%.in" * [ test ] }
+
