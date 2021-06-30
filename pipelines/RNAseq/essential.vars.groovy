ef865f8be99f69be9bb55f9a0d1eef75e0eb942d
Emil Karaulanov <e.karaulanov@imb-mainz.de>
2021-06-30 13:05:36 +0000

    Update essential.vars.groovy

--- pipelines/RNAseq/essential.vars.groovy
+++ pipelines/RNAseq/essential.vars.groovy
@@ -1,26 +1,47 @@
-ESSENTIAL_PROJECT="/project/"
-ESSENTIAL_STAR_REF="/annotation/mm9/star_genome"
-ESSENTIAL_GENESGTF="/annotation/mm9/gencode.vM1.annotation.gtf"
-ESSENTIAL_GENESBED="/annotation/mm9/mm9_UCSC_knownGene.bed"
-ESSENTIAL_CHROMSIZES="/annotation/mm9/mm9.chrom.sizes"  // chromosome sizes file
-ESSENTIAL_FEATURETYPE="gene_type" //gencode uses gene_type; ensemble uses gene_biotype
-ESSENTIAL_SAMPLE_PREFIX=""
-ESSENTIAL_PAIRED="no"           // paired end design
-ESSENTIAL_STRANDED="reverse"    // strandness: no|yes|reverse
-ESSENTIAL_ORG="mouse"           // UCSC organism
-ESSENTIAL_DB="mm9"              // UCSC assembly version
-ESSENTIAL_READLENGTH=50         // added for STAR version > 2.4.1a
+// RNA-Seq ESSENTIAL VARIABLES
+
+// Define essential variables here.
+// Further module-specific variables can be adjusted in the corresponding ".header" files for each module.
+
+// General parameters
+ESSENTIAL_PROJECT="/project/"   // full project directory path
 ESSENTIAL_THREADS=4             // number of threads for parallel tasks
-ESSENTIAL_FRAGMENT_USAGE="no"   // should fragments be reconstituted? should always be no for rnaseq
-ESSENTIAL_FRAGLEN=200           // mean length of library inserts (default 200)
-ESSENTIAL_FILTER_CHR=""         // chromosomes to include in post-mapping analysis.
-ESSENTIAL_BAMCOVERAGE="--binSize 1 --skipNonCoveredRegions --normalizeUsing CPM" // NO smoothing should be done for RNAseq
-// size of bins (in bases)  and method to normalize the number of reads per bin to generate bigwig file.
+ESSENTIAL_SAMPLE_PREFIX=""      // sample name prefix to be trimmed in the results and reports
+
+// Mapping and annotation parameters
+ESSENTIAL_STAR_REF="..../star/2.7.3a"     // directory containing all STAR index files
+ESSENTIAL_GENESGTF="..../annotation.gtf"  // properly formatted gene annotation GTF file having *different* gene and trancript IDs (e.g. from Ensembl or Gencode)
+ESSENTIAL_GENESBED="..../annotation.bed"  // gene annotation BED file 
+ESSENTIAL_CHROMSIZES="..../genome.fa.fai" // chromosome sizes file
+ESSENTIAL_FEATURETYPE="gene_type"         // Gencode uses "gene_type", Ensembl uses "gene_biotype"
+ESSENTIAL_PAIRED="no"           // single-end ("no") or paired-end ("yes") data
+ESSENTIAL_READLENGTH=50         // read length - used for optimal mapping over splice-junctions
+ESSENTIAL_STRANDED="reverse"    // library prep protocol strandness: no|yes|reverse
+ESSENTIAL_ORG="mouse"           // UCSC organism name
+ESSENTIAL_DB="mm9"              // UCSC assembly version
+ESSENTIAL_FILTER_CHR=""         // chromosomes to include in post-mapping analysis
+ESSENTIAL_BAMCOVERAGE="--binSize 1 --skipNonCoveredRegions --normalizeUsing CPM"  // size of bins (in bp) and normalisation method for the bigWig tracks
+
+// DESeq2 specific parameters
+ESSENTIAL_DESEQ2_FDR=0.01       // FDR significance cutoff in the DESeq2 model (may be increased for noisy or underpowered datasets)
+ESSENTIAL_DESEQ2_FC=1           // optional Fold-Change cutoff to incorporate into the DESeq2 model (default "FC=1" means no Fold-Change filter is used)
+// Using FC threshold in the DESeq2 model is usually more conservative than post-hoc gene filtering by FC (which should anyway be avoided, see https://doi.org/10.1093/bib/bbab053) 
+
+// Optional pipeline stages to include
+RUN_TRACKHUB=false
+RUN_FASTQSCREEN=true
+
+// FASTQ-Screen parameters
 ESSENTIAL_FASTQSCREEN_PERC=1    // contaminant filter, if a contaminant is consuming at least this percentage of reads in at least one sample, contaminant will be shown in report
 ESSENTIAL_FASTQSCREEN_GENOME="Yeast::/fsimb/common/genomes/saccharomyces_cerevisiae/ensembl/R64/canonical/index/bowtie2/2.3.2/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel"  //bowtie2 reference for the genome the samples are from, this is used for the fastqscreen
 ESSENTIAL_FASTQSCREEN=ESSENTIAL_FASTQSCREEN_GENOME + ",PHIX::/fsimb/common/genomes/phix/19930428/NCBI/index/bowtie2/2.3.4.3/ncbi_phix,ERCC::/fsimb/common/genomes/ERCC/index/bowtie2/2.3.4.3/ERCC92,rRNA::/fsimb/common/genomes/contaminants/fastqscreen_references/rrna/v1/index/bowtie2/2.3.4.3/hs_mm_ce_dm_rn_dr_xt_rRNA,Mycoplasma::/fsimb/common/genomes/contaminants/fastqscreen_references/mycoplasma/v1/index/bowtie2/2.3.4.3/mycoplasma_all_ref,E.coli::/fsimb/common/genomes/Escherichia_coli/ensembl/full/index/bowtie2/Escherichia_coli_str_k_12_substr_dh10b.ASM1942v1.31.dna.genome,B.taurus::/fsimb/common/genomes/bos_taurus/ensembl/3.1/full/index/bowtie2/2.2.9/UMD3.1" //references for fastqscreen to use if run, this are our standard references please include yours 
 
-//global vars
+// Other parameters
+ESSENTIAL_FRAGMENT_USAGE="no"   // should fragments be reconstituted? should always be "no" for RNAseq data
+ESSENTIAL_FRAGLEN=200           // mean length of library inserts (default 200)
+RUN_IN_PAIRED_END_MODE=(ESSENTIAL_PAIRED == "yes")
+
+//project folders
 PROJECT=ESSENTIAL_PROJECT
 LOGS=PROJECT + "/logs"
 MAPPED=PROJECT + "/mapped"
@@ -30,11 +51,3 @@ RESULTS=PROJECT + "/results"
 TMP=PROJECT + "/tmp"
 TRACKS=PROJECT + "/tracks"
 FUSION=PROJECT + "/fusion"
-
-//DESeq2 specific vars
-ESSENTIAL_DESEQ2_FDR=0.01 // FDR filter used to retrieve the results in DESeq2
-ESSENTIAL_DESEQ2_FC=1     // FC filter used to retrieve the result in DESeq2 (non log2)!
-// optional pipeline stages to include
-RUN_TRACKHUB=false
-RUN_IN_PAIRED_END_MODE=(ESSENTIAL_PAIRED == "yes")
-RUN_FASTQSCREEN=true
