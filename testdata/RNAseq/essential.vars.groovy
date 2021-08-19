7bb7c01363be7adfa544fb05448aa899f96f0137
skarunan <skarunan@hpc1.IMB.Uni-Mainz.DE>
2021-08-19 13:45:22 +0200

    Update testdata settings to reflect cutadapt and DESeq mmatrix changes

--- testdata/RNAseq/essential.vars.groovy
+++ testdata/RNAseq/essential.vars.groovy
@@ -19,6 +19,10 @@ ESSENTIAL_BAMCOVERAGE="--binSize 1 --skipNonCoveredRegions --normalizeUsing CPM"
 ESSENTIAL_FASTQSCREEN_PERC=1    // contaminant filter, if a contaminant is consuming at least this percentage of reads in at least one sample, contaminant will be shown in report
 ESSENTIAL_FASTQSCREEN_GENOME="Yeast::/fsimb/common/genomes/saccharomyces_cerevisiae/ensembl/R64/canonical/index/bowtie2/2.3.2/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel"  //bowtie2 reference for the genome the samples are from, this is used for the fastqscreen
 ESSENTIAL_FASTQSCREEN=ESSENTIAL_FASTQSCREEN_GENOME + ",PHIX::/fsimb/common/genomes/phix/19930428/NCBI/index/bowtie2/2.3.4.3/ncbi_phix,ERCC::/fsimb/common/genomes/ERCC/index/bowtie2/2.3.4.3/ERCC92,rRNA::/fsimb/common/genomes/contaminants/fastqscreen_references/rrna/v1/index/bowtie2/2.3.4.3/hs_mm_ce_dm_rn_dr_xt_rRNA,Mycoplasma::/fsimb/common/genomes/contaminants/fastqscreen_references/mycoplasma/v1/index/bowtie2/2.3.4.3/mycoplasma_all_ref,E.coli::/fsimb/common/genomes/Escherichia_coli/ensembl/full/index/bowtie2/Escherichia_coli_str_k_12_substr_dh10b.ASM1942v1.31.dna.genome,B.taurus::/fsimb/common/genomes/bos_taurus/ensembl/3.1/full/index/bowtie2/2.2.9/UMD3.1" //references for fastqscreen to use if run, this are our standard references please include yours 
+// Adapter trimming with Cutadapt (additional adapter sequences for R1 and/or R2 can be specified in the cutadapt.header file)         
+ESSENTIAL_ADAPTER_SEQUENCE="Nextera=CTGTCTCTTATACACATCT" // 3'adapter sequence for R1. 
+ESSENTIAL_MINADAPTEROVERLAP=5
+ESSENTIAL_MINREADLENGTH=30
 
 //global vars
 PROJECT=ESSENTIAL_PROJECT
@@ -30,6 +34,7 @@ RESULTS=PROJECT + "/results"
 TMP=PROJECT + "/tmp"
 TRACKS=PROJECT + "/tracks"
 FUSION=PROJECT + "/fusion"
+TRIMMED=PROJECT + "/trimmed"
 
 //DESeq2 specific vars
 ESSENTIAL_DESEQ2_FDR=0.01 // FDR filter used to retrieve the results in DESeq2
@@ -38,3 +43,4 @@ ESSENTIAL_DESEQ2_FC=1     // FC filter used to retrieve the result in DESeq2 (no
 RUN_TRACKHUB=false
 RUN_IN_PAIRED_END_MODE=(ESSENTIAL_PAIRED == "yes")
 RUN_FASTQSCREEN=true
+RUN_CUTADAPT=true
