1bdfe599660504bf410da4b927955840448decb2
Giuseppe Petrosino <g.petrosino@imb-mainz.de>
2022-05-16 17:57:15 +0200

    Update strandSpecificBW.groovy

--- modules/NGS/strandSpecificBW.groovy
+++ modules/NGS/strandSpecificBW.groovy
@@ -10,21 +10,21 @@ strandBigWig = {
     //this might be confusing regarding the reverse and forward
     //setting but according to the deeptools manual it has to be like this
     if(strandBigWig_vars.stranded == "yes") {
-        def FORWARD="reverse"
-        def REVERSE="forward"
+       strandBigWig_vars.forward="reverse"
+       strandBigWig_vars.reverse="forward"
     } else if(strandBigWig_vars.stranded == "reverse") {
-        def FORWARD="forward"
-        def REVERSE="reverse"
+       strandBigWig_vars.forward="forward"
+       strandBigWig_vars.reverse="reverse"
     }
 
     // if you use > v3 modify the vars to --normalizeUsing RPKM since the API of deeptools changed
     def STRANDBIGWIG_FLAGS =
         (strandBigWig_vars.threads               ? " --numberOfProcessors " + strandBigWig_vars.threads       : "") +
-        (strandBigWig_vars.smoothLength          ? " --smoothLength "       + strandBigWig_vars.smoothLength  : "") +
         (strandBigWig_vars.binSize               ? " --binSize "            + strandBigWig_vars.binSize       : "") +
-        (strandBigWig_vars.normalizeUsingRPKM    ? " --normalizeUsingRPKM"                                    : "") +
+        (strandBigWig_vars.normalizeUsing        ? " --normalizeUsing "      + strandBigWig_vars.normalizeUsing : "") +
         (strandBigWig_vars.skipNonCoveredRegions ? " --skipNonCoveredRegions"                                 : "") +
-        (strandBigWig_vars.extra                 ? " "                      + strandBigWig_vars.extra         : "")
+        (strandBigWig_vars.outFileFormat         ? " --outFileFormat "      + strandBigWig_vars.outFileFormat : "") +
+        (strandBigWig_vars.extra                 ? ""                      + strandBigWig_vars.extra         : "")
 
     def TOOL_ENV = prepare_tool_env("deeptools", tools["deeptools"]["version"], tools["deeptools"]["runenv"]) + " && " +
                    prepare_tool_env("kentutils", tools["kentutils"]["version"], tools["kentutils"]["runenv"]) + " && " +
@@ -40,13 +40,15 @@ strandBigWig = {
             echo \$base &&
             CHRSIZES=\${TMP}/\${base}.bam2bw.chrsizes &&
             samtools idxstats ${input} | cut -f1-2 > \${CHRSIZES} &&
-            bamCoverage --filterRNAstrand $FORWARD $STRANDBIGWIG_FLAGS -b $input -o \${TMP}/\${base}.fwd.bedgraph &&
-            bamCoverage --filterRNAstrand $REVERSE $STRANDBIGWIG_FLAGS -b $input -o \${TMP}/\${base}.rev.bedgraph &&
+            bamCoverage --filterRNAstrand ${strandBigWig_vars.forward} $STRANDBIGWIG_FLAGS -b $input -o \${TMP}/\${base}.fwd.bedgraph &&
+            bamCoverage --filterRNAstrand ${strandBigWig_vars.reverse} $STRANDBIGWIG_FLAGS -b $input -o \${TMP}/\${base}.rev.bedgraph &&
 
             awk 'BEGIN {OFS="\t"; FS="\t"}{print \$1,\$2, \$3,"-"\$4}' \${TMP}/\${base}.rev.bedgraph > \${TMP}/\${base}.rev.bedgraph_neg &&
             mv \${TMP}/\${base}.rev.bedgraph_neg \${TMP}/\${base}.rev.bedgraph &&
-            bedGraphToBigWig \${TMP}/\${base}.fwd.bedgraph $CHRSIZES $output1 &&
-            bedGraphToBigWig \${TMP}/\${base}.rev.bedgraph $CHRSIZES $output2
+            sort -k1,1 -k2,2n \${TMP}/\${base}.fwd.bedgraph > \${TMP}/\${base}.fwd.sorted.bedgraph &&
+            sort -k1,1 -k2,2n \${TMP}/\${base}.rev.bedgraph > \${TMP}/\${base}.rev.sorted.bedgraph &&
+            bedGraphToBigWig \${TMP}/\${base}.fwd.sorted.bedgraph $CHRSIZES $output1 &&
+            bedGraphToBigWig \${TMP}/\${base}.rev.sorted.bedgraph $CHRSIZES $output2
         ""","strandBigWig"
     }
     forward input
