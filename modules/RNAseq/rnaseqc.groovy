1832813c0f03da6eab8d573ed6a63d7532dbea74
Sivarajan Karunanithi <s.karunanithi@imb-mainz.de>
2022-10-06 10:20:13 +0200

    Adding modules for RNASeQC - Used mainly for reprocessing public data sets similar to gtex processing

---
+++ modules/RNAseq/rnaseqc.groovy
@@ -0,0 +1,26 @@
+rnaseqc = {
+	doc title: "RNA-SeQC: Basic quality control for RNA-seq",
+	desc: "efficient RNA-seq quality control and quantification for large cohorts",
+	constraints: "",
+	author: "Sivarajan Karunanithi"
+
+	output.dir = rnaseqc_vars.outdir
+	def RNASEQC_FLAGS = 
+		(rnaseqc_vars.legacy ? " --legacy" : "") + 
+		(rnaseqc_vars.extra    ? " " + rnaseqc_vars.extra : "")
+
+	def TOOL_ENV = prepare_tool_env("rnaseqc", tools["rnaseqc"]["version"], tools["rnaseqc"]["runenv"])
+	def PREAMBLE = get_preamble(stage: stageName, outdir: output.dir, input: new File(input1.prefix).getName())
+
+	//run the chunk
+	transform(".bam") to (".bam.gene_reads.gct") {
+	exec """
+		${TOOL_ENV} &&
+		${PREAMBLE} &&
+		
+		rnaseqc ${rnaseqc_vars.gtf} $input $output.dir ${RNASEQC_FLAGS} &&
+		rm $output.dir/*.gene_tpm.gct $output.dir/*.exon_cv.tsv $output.dir/*.exon_reads.gct $output.dir/*.gene_fragments.gct $output.dir/*.metrics.tsv;
+	""","rnaseqc"
+
+	}
+}
