3cdc2681dc8e48bd5210bad962f44b88e4ac5cd8
frruehle <f.ruehle@imb-mainz.de>
2025-08-20 09:36:48 +0200

    add findmarkers_bioc module to scRNAseq.pipeline.groovy

--- pipelines/scRNAseq/scRNA.pipeline.groovy
+++ pipelines/scRNAseq/scRNA.pipeline.groovy
@@ -33,6 +33,7 @@ load PIPELINE_ROOT + "/modules/scRNAseq/igraph_bioc.header"
 load PIPELINE_ROOT + "/modules/scRNAseq/hclust_bioc.header"
 load PIPELINE_ROOT + "/modules/scRNAseq/kmeans_bioc.header"
 load PIPELINE_ROOT + "/modules/scRNAseq/collectCl_bioc.header"
+load PIPELINE_ROOT + "/modules/scRNAseq/findmarkers_bioc.header"
 load PIPELINE_ROOT + "/modules/NGS/bamcoverage.header"
 load PIPELINE_ROOT + "/modules/NGS/bamindexer.header"
 load PIPELINE_ROOT + "/modules/NGS/fastqc.header"
@@ -80,7 +81,7 @@ Bpipe.run {
     				    dontrun.using(module:"Sample aggregation. Sequencing type not found!") )))) ] :
     dontrun.using(module:"module readIndivSamplesAndMerge_bioc not implemented yet!") ) +
 
-    qc_bioc + filtCells_bioc + norm_bioc + [igraph_bioc, hclust_bioc, kmeans_bioc] + collectCl_bioc +
+    qc_bioc + filtCells_bioc + norm_bioc + [igraph_bioc, hclust_bioc, kmeans_bioc] + collectCl_bioc + findmarkers_bioc +
 	
     (RUN_TRACKHUB ? trackhub_config + trackhub : dontrun.using(module:"trackhub")) +
     collectToolVersions + MultiQC + 
